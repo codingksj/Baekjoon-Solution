6bf2955e757a5bbf40624741fda088acb904dead
codingksj <45532564+codingksj@users.noreply.github.com>
2023-08-03 19:47:34 +0900

    Add 05338.cpp

---
+++ 백준/05000~05999/05300~05399/05338.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<set>
+#include<map>
+#include<vector>
+#include<queue>
+#include<deque>
+#include<algorithm>
+#include<string>
+#include<numeric>
+#include<cmath>
+#include<iomanip>
+
+using namespace std;
+
+typedef long long LL;
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	cout << "       _.-;;-._\n" 
+		<< "'-..-'|   ||   |\n" 
+		<< "'-..-'|_.-;;-._|\n" 
+		<< "'-..-'|   ||   |\n" 
+		<< "'-..-'|_.-''-._|\n";
+	return 0;
+}
