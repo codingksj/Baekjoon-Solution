67e4265668784f81a86d7fdb5a6599eeb4c0df69
codingksj <45532564+codingksj@users.noreply.github.com>
2023-08-03 19:48:18 +0900

    Add 10815.c

---
+++ 백준/10000~10999/10800~10899/10815.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#define MAX 20000002
+#define T 10000000
+int cards[MAX];
+
+int main()
+{
+    int M, N;
+    int tmp;
+    scanf("%d",&N);
+    for(int i=0;i<N;i++){
+        scanf("%d",&tmp);
+        cards[tmp+T]=1;
+    }
+    scanf("%d",&M);
+    for(int i=0;i<M;i++){
+        scanf("%d",&tmp);
+        printf("%d ",cards[tmp+T]);
+    }
+    return 0;
+}
