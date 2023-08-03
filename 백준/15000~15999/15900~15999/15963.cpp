b1674ee0991a49cc017f767c512a1f40a4350a88
codingksj <45532564+codingksj@users.noreply.github.com>
2023-08-03 19:48:54 +0900

    Add 15963.cpp

---
+++ 백준/15000~15999/15900~15999/15963.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<set>
+#include<unordered_map>
+#include<map>
+#include<vector>
+#include<queue>
+#include<deque>
+#include<stack>
+#include<algorithm>
+#include<string>
+#include<numeric>
+#include<cmath>
+#include<cctype>
+#include<cstring>
+#include<iomanip>
+#include<chrono>
+
+using namespace std;
+
+typedef long long LL;
+typedef pair<int, int> Pii;
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	LL N, M;
+	cin >> N >> M;
+	cout << (N == M);
+	return 0;
+}
