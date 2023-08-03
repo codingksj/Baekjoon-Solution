07c0fc819f75ad4a6f55ed238cd742d7e6df7cdf
codingksj <45532564+codingksj@users.noreply.github.com>
2023-08-03 19:47:57 +0900

    Add 08871.cpp

---
+++ 백준/08000~08999/08800~08899/08871.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<set>
+#include<map>
+#include<vector>
+#include<queue>
+#include<deque>
+#include<algorithm>
+#include<string>
+#include<numeric>
+#include<cmath>
+#include<iomanip>
+
+using namespace std;
+
+typedef long long LL;
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	int N;
+	cin >> N;
+	cout << 2 * (N + 1) << "\n" 
+		<< 3 * (N + 1) << "\n";
+	return 0;
+}
