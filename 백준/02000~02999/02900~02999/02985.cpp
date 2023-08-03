12b908e7a4d2ddcb1f8f724cb171ce005fa15bab
codingksj <45532564+codingksj@users.noreply.github.com>
2023-08-03 19:47:00 +0900

    Add 02985.cpp

---
+++ 백준/02000~02999/02900~02999/02985.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<set>
+#include<unordered_map>
+#include<map>
+#include<vector>
+#include<queue>
+#include<deque>
+#include<stack>
+#include<algorithm>
+#include<string>
+#include<numeric>
+#include<cmath>
+#include<cctype>
+#include<cstring>
+#include<iomanip>
+#include<chrono>
+
+using namespace std;
+
+typedef long long LL;
+typedef pair<int, int> Pii;
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	
+    double a, b, c;
+    cin >> a >> b >> c;
+    if (a + b == c)
+        cout << a << '+' << b << '=' << c;
+    else if (a - b == c)
+        cout << a << '-' << b << '=' << c;
+    else if (a * b == c)
+        cout << a << '*' << b << '=' << c;
+    else if (a / b == c)
+        cout << a << '/' << b << '=' << c;
+    else if (a == b + c)
+        cout << a << '=' << b << '+' << c;
+    else if (a == b - c)
+        cout << a << '=' << b << '-' << c;
+    else if (a == b * c)
+        cout << a << '=' << b << '*' << c;
+    else if (a == b / c)
+        cout << a << '=' << b << '/' << c;
+	return 0;
+}
