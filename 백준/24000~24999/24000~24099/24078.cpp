b7bec7538cfaf4d0f0cb7e71c6483361173ec669
codingksj <45532564+codingksj@users.noreply.github.com>
2023-08-03 19:49:20 +0900

    Add 24078.cpp

---
+++ 백준/24000~24999/24000~24099/24078.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<set>
+#include<map>
+#include<vector>
+#include<queue>
+#include<deque>
+#include<algorithm>
+#include<string>
+#include<numeric>
+#include<cmath>
+#include<cctype>
+#include<iomanip>
+
+using namespace std;
+
+typedef long long LL;
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	int X;
+	cin >> X;
+	cout << X % 21;
+	return 0;
+}
