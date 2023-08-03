a9c8229c18752fa0df0efb0279664ae087d4a8ff
codingksj <45532564+codingksj@users.noreply.github.com>
2023-08-03 19:47:12 +0900

    Add 03765.cpp

---
+++ 백준/03000~03999/03700~03799/03765.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<set>
+#include<map>
+#include<vector>
+#include<queue>
+#include<deque>
+#include<algorithm>
+#include<string>
+#include<numeric>
+#include<cmath>
+#include<cctype>
+#include<cstring>
+#include<iomanip>
+
+using namespace std;
+
+typedef long long LL;
+typedef pair<int, int> Pii;
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	string line;
+	while (getline(cin, line)) {
+		cout << line << "\n";
+	}
+	return 0;
+}
