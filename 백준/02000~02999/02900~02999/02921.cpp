102b9e10d69732f8e732db4d8955730e9439986f
codingksj <45532564+codingksj@users.noreply.github.com>
2023-08-03 19:46:57 +0900

    Add 02921.cpp

---
+++ 백준/02000~02999/02900~02999/02921.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<set>
+#include<unordered_map>
+#include<map>
+#include<vector>
+#include<queue>
+#include<deque>
+#include<stack>
+#include<algorithm>
+#include<string>
+#include<numeric>
+#include<cmath>
+#include<cctype>
+#include<cstring>
+#include<iomanip>
+#include<chrono>
+
+using namespace std;
+
+typedef long long LL;
+typedef pair<int, int> Pii;
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	
+	LL n;
+	cin >> n;
+	cout << n * (n + 1) * (n + 2) / 2;
+	return 0;
+}
