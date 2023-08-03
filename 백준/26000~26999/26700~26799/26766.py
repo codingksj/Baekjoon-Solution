7f0e616bbd5cb4307a6127dbf40d6c0c1bfa9893
codingksj <45532564+codingksj@users.noreply.github.com>
2023-08-03 19:49:28 +0900

    Add 26766.py

---
+++ 백준/26000~26999/26700~26799/26766.py
@@ -0,0 +1,14 @@
+lines = [
+    " @@@   @@@ \n",
+    "@   @ @   @\n",
+    "@    @    @\n",
+    "@         @\n",
+    " @       @ \n",
+    "  @     @  \n",
+    "   @   @   \n",
+    "    @ @    \n",
+    "     @     \n",
+]
+heart = "".join(lines)
+n = int(input())
+print(heart*n)
