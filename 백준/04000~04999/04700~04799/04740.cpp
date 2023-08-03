297aa543623bb72beff93da861aa6b6da9b52fb5
codingksj <45532564+codingksj@users.noreply.github.com>
2023-08-03 19:47:24 +0900

    Add 04740.cpp

---
+++ 백준/04000~04999/04700~04799/04740.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<set>
+#include<unordered_map>
+#include<map>
+#include<vector>
+#include<queue>
+#include<deque>
+#include<stack>
+#include<algorithm>
+#include<string>
+#include<numeric>
+#include<cmath>
+#include<cctype>
+#include<cstring>
+#include<iomanip>
+#include<chrono>
+
+using namespace std;
+
+typedef long long LL;
+typedef pair<int, int> Pii;
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	string line;
+	while (true) {
+		getline(cin, line);
+		if (line == "***") {
+			break;
+		}
+		reverse(line.begin(), line.end());
+		cout << line << "\n";
+	}
+	return 0;
+}
