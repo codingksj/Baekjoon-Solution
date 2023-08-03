ee3658233e8414a96f6023d6e3d888cc946a51d9
codingksj <45532564+codingksj@users.noreply.github.com>
2023-08-03 19:49:18 +0900

    Add 21598.cpp

---
+++ 백준/21000~21999/21500~21599/21598.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<set>
+#include<map>
+#include<vector>
+#include<queue>
+#include<deque>
+#include<algorithm>
+#include<string>
+#include<numeric>
+#include<cmath>
+#include<cctype>
+#include<iomanip>
+
+using namespace std;
+
+typedef long long LL;
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	int TC;
+	string sentence = "SciComLove";
+	cin >> TC;
+	while (TC--) {
+		cout << sentence << "\n";
+	}
+	return 0;
+}
