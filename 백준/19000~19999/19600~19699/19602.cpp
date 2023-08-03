2127da90e97cd44cbdec12f4b03a03e0847e3e42
codingksj <45532564+codingksj@users.noreply.github.com>
2023-08-03 19:49:11 +0900

    Add 19602.cpp

---
+++ 백준/19000~19999/19600~19699/19602.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<set>
+#include<unordered_map>
+#include<map>
+#include<vector>
+#include<queue>
+#include<deque>
+#include<stack>
+#include<algorithm>
+#include<string>
+#include<numeric>
+#include<cmath>
+#include<cctype>
+#include<cstring>
+#include<iomanip>
+#include<chrono>
+
+using namespace std;
+
+typedef long long LL;
+typedef pair<int, int> Pii;
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	int S, M, L;
+	bool happy;
+
+	cin >> S >> M >> L;
+	happy = (S + 2 * M + 3 * L) >= 10;
+	cout << (happy ? "happy" : "sad");
+
+	return 0;
+}
