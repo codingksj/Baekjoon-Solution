3c9ccc30babe355ab6c3d5d419feac5370f58c47
codingksj <45532564+codingksj@users.noreply.github.com>
2023-08-03 19:48:09 +0900

    Add 10170.cpp

---
+++ 백준/10000~10999/10100~10199/10170.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<set>
+#include<map>
+#include<vector>
+#include<queue>
+#include<deque>
+#include<algorithm>
+#include<string>
+#include<numeric>
+#include<cmath>
+#include<iomanip>
+
+using namespace std;
+
+typedef long long LL;
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+    cout << "NFC West       W   L  T\n"
+        << "-----------------------\n"
+        << "Seattle        13  3  0\n"
+        << "San Francisco  12  4  0\n"
+        << "Arizona        10  6  0\n"
+        << "St. Louis      7   9  0\n\n"
+        << "NFC North      W   L  T\n"
+        << "-----------------------\n"
+        << "Green Bay      8   7  1\n"
+        << "Chicago        8   8  0\n"
+        << "Detroit        7   9  0\n"
+        << "Minnesota      5  10  1";
+	return 0;
+}
