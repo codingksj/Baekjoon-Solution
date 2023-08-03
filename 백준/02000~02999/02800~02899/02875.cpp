979565ca0e0d55823c130905ae3efe4d63612bd0
codingksj <45532564+codingksj@users.noreply.github.com>
2023-08-03 19:46:54 +0900

    Add 02875.cpp

---
+++ 백준/02000~02999/02800~02899/02875.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<set>
+#include<unordered_map>
+#include<map>
+#include<vector>
+#include<queue>
+#include<deque>
+#include<stack>
+#include<algorithm>
+#include<string>
+#include<numeric>
+#include<cmath>
+#include<cctype>
+#include<cstring>
+#include<iomanip>
+#include<chrono>
+
+using namespace std;
+
+typedef long long LL;
+typedef pair<int, int> Pii;
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	
+	int maxTeam;
+	int N, M, K;
+	cin >> N >> M >> K;
+
+	maxTeam = min(min(M, N / 2), (N + M - K) / 3);
+	cout << maxTeam;
+
+	return 0;
+}
