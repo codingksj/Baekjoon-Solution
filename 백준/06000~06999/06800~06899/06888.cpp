f0ce5dd3548342cc217dd450ab9547a663bb5545
codingksj <45532564+codingksj@users.noreply.github.com>
2023-08-03 19:47:47 +0900

    Add 06888.cpp

---
+++ 백준/06000~06999/06800~06899/06888.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<set>
+#include<unordered_map>
+#include<map>
+#include<vector>
+#include<queue>
+#include<deque>
+#include<stack>
+#include<algorithm>
+#include<string>
+#include<numeric>
+#include<cmath>
+#include<cctype>
+#include<cstring>
+#include<iomanip>
+#include<chrono>
+
+using namespace std;
+
+typedef long long LL;
+typedef pair<int, int> Pii;
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	LL X, Y;
+	cin >> X >> Y;
+	while (X <= Y) {
+		cout << "All positions change in year" << " " << X << "\n";
+		X += 60;
+	}
+	return 0;
+}
