e533213211e49bee87a128148f90fa0616082c01
codingksj <45532564+codingksj@users.noreply.github.com>
2023-08-03 19:48:33 +0900

    Add 11549.cpp

---
+++ 백준/11000~11999/11500~11599/11549.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<set>
+#include<unordered_map>
+#include<map>
+#include<vector>
+#include<queue>
+#include<deque>
+#include<stack>
+#include<algorithm>
+#include<string>
+#include<numeric>
+#include<cmath>
+#include<cctype>
+#include<cstring>
+#include<iomanip>
+#include<chrono>
+
+using namespace std;
+
+typedef long long LL;
+typedef pair<int, int> Pii;
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	int answer, tea, count = 0;
+	cin >> answer;
+	for (int i = 0; i < 5; i++) {
+		cin >> tea;
+		(answer == tea) && count++;
+	}
+	cout << count;
+	return 0;
+}
