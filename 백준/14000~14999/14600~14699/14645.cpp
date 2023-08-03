6f4dd5be940ac40257b2b7abd51e7be89573f571
codingksj <45532564+codingksj@users.noreply.github.com>
2023-08-03 19:48:43 +0900

    Add 14645.cpp

---
+++ 백준/14000~14999/14600~14699/14645.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<set>
+#include<map>
+#include<vector>
+#include<queue>
+#include<deque>
+#include<algorithm>
+#include<string>
+#include<numeric>
+#include<cmath>
+#include<iomanip>
+
+using namespace std;
+
+typedef long long LL;
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	int TC, K;
+	int in, out;
+	cin >> TC >> K;
+	while (TC--) {
+		cin >> in >> out;
+	}
+	cout << "비와이\n";
+	return 0;
+}
