5ab691c470709180079376beee8aecdab1b9eb7a
codingksj <45532564+codingksj@users.noreply.github.com>
2023-08-03 19:49:24 +0900

    Add 26082.cpp

---
+++ 백준/26000~26999/26000~26099/26082.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<set>
+#include<map>
+#include<vector>
+#include<queue>
+#include<deque>
+#include<algorithm>
+#include<string>
+#include<numeric>
+#include<cmath>
+#include<cctype>
+#include<cstring>
+#include<iomanip>
+
+using namespace std;
+
+typedef long long LL;
+typedef pair<int, int> Pii;
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	int A, B, C, X;
+	cin >> A >> B >> C;
+	X = 3 * B * C / A;
+	cout << X;
+	return 0;
+}
