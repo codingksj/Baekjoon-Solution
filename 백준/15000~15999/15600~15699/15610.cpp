3a90bf81808f46ebbf355637175c23baefe58586
codingksj <45532564+codingksj@users.noreply.github.com>
2023-08-03 19:48:50 +0900

    Add 15610.cpp

---
+++ 백준/15000~15999/15600~15699/15610.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<set>
+#include<unordered_map>
+#include<map>
+#include<vector>
+#include<queue>
+#include<deque>
+#include<stack>
+#include<algorithm>
+#include<string>
+#include<numeric>
+#include<cmath>
+#include<cctype>
+#include<cstring>
+#include<iomanip>
+#include<chrono>
+
+using namespace std;
+
+typedef long long LL;
+typedef pair<int, int> Pii;
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	double length;
+	cin >> length;
+	cout.precision(12);
+	cout << 4.0 * sqrt(length);
+	return 0;
+}
