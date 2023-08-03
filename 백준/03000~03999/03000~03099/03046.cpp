7bcc6002141a8ae446d0a8343ffc701544b854b4
codingksj <45532564+codingksj@users.noreply.github.com>
2023-08-03 19:47:05 +0900

    Add 03046.cpp

---
+++ 백준/03000~03999/03000~03099/03046.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<set>
+#include<map>
+#include<vector>
+#include<queue>
+#include<deque>
+#include<algorithm>
+#include<string>
+#include<numeric>
+#include<cmath>
+#include<cctype>
+#include<cstring>
+#include<iomanip>
+
+using namespace std;
+
+typedef long long LL;
+typedef pair<int, int> Pii;
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	int R1, R2, S;
+	cin >> R1 >> S;
+	R2 = 2 * S - R1;
+	cout << R2;
+	return 0;
+}
