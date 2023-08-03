5d3576e49117f35aa18490a5172fe730add22649
codingksj <45532564+codingksj@users.noreply.github.com>
2023-08-03 19:47:42 +0900

    Add 05717.cpp

---
+++ 백준/05000~05999/05700~05799/05717.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<set>
+#include<unordered_map>
+#include<map>
+#include<vector>
+#include<queue>
+#include<deque>
+#include<stack>
+#include<algorithm>
+#include<string>
+#include<numeric>
+#include<cmath>
+#include<cctype>
+#include<cstring>
+#include<iomanip>
+#include<chrono>
+
+using namespace std;
+
+typedef long long LL;
+typedef pair<int, int> Pii;
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	int M, F;
+	while (true) {
+		cin >> M >> F;
+		if (M == 0 && F == 0) {
+			break;
+		}
+		cout << M + F << "\n";
+	}
+	return 0;
+}
