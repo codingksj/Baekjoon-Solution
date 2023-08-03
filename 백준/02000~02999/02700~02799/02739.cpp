75513ee7c02115a30a4407b7d5bb3285b9ccb560
codingksj <45532564+codingksj@users.noreply.github.com>
2023-08-03 19:46:48 +0900

    Add 02739.cpp

---
+++ 백준/02000~02999/02700~02799/02739.cpp
@@ -0,0 +1,14 @@
+#include <iostream> 
+
+using namespace std;
+
+int main(void) {
+
+    int num;
+   
+    cin >> num; 
+    for (int i = 1; i < 10; i++) {
+        cout << num << " * " << i << " = " << num * i << "\n";
+    }
+    return 0;
+}
