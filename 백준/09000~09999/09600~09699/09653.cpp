113f1bd5ceb737a08b9647c7a7a8cfac0a0a551c
codingksj <45532564+codingksj@users.noreply.github.com>
2023-08-03 19:48:04 +0900

    Add 09653.cpp

---
+++ 백준/09000~09999/09600~09699/09653.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<set>
+#include<map>
+#include<vector>
+#include<queue>
+#include<deque>
+#include<algorithm>
+#include<string>
+#include<numeric>
+#include<cmath>
+
+using namespace std;
+
+typedef long long LL;
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	string starwars = 
+		"    8888888888  888    88888\n"
+		"   88     88   88 88   88  88\n"
+		"    8888  88  88   88  88888\n"
+		"       88 88 888888888 88   88\n"
+		"88888888  88 88     88 88    888888\n"
+		"\n"
+		"88  88  88   888    88888    888888\n"
+		"88  88  88  88 88   88  88  88\n"
+		"88 8888 88 88   88  88888    8888\n"
+		" 888  888 888888888 88  88      88\n"
+		"  88  88  88     88 88   88888888\n";
+	cout << starwars;
+	return 0;
+}
