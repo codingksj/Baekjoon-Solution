2e0d9c6c006835c24c07df116675850b807102c0
codingksj <45532564+codingksj@users.noreply.github.com>
2023-08-03 19:48:37 +0900

    Add 13136.cpp

---
+++ 백준/13000~13999/13100~13199/13136.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<set>
+#include<unordered_map>
+#include<map>
+#include<vector>
+#include<queue>
+#include<deque>
+#include<stack>
+#include<algorithm>
+#include<string>
+#include<numeric>
+#include<cmath>
+#include<cctype>
+#include<cstring>
+#include<iomanip>
+#include<chrono>
+
+using namespace std;
+
+typedef long long LL;
+typedef pair<int, int> Pii;
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	LL R, C, N;
+	LL cctv;
+	cin >> R >> C >> N;
+	cctv = ceil((double)R / N)* ceil((double)C / N);
+	cout << cctv;
+	return 0;
+}
