7283d8aa21ccc19e66b147466254bbd59cb2392f
codingksj <45532564+codingksj@users.noreply.github.com>
2023-08-03 19:49:25 +0900

    Add 26307.cpp

---
+++ 백준/26000~26999/26300~26399/26307.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<set>
+#include<map>
+#include<vector>
+#include<queue>
+#include<deque>
+#include<algorithm>
+#include<string>
+#include<numeric>
+#include<cmath>
+#include<cctype>
+#include<cstring>
+#include<iomanip>
+
+using namespace std;
+
+typedef long long LL;
+typedef pair<int, int> Pii;
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	int hour, minute;
+	int total_minute;
+	cin >> hour >> minute;
+	total_minute = (hour - 9) * 60 + minute;
+	cout << total_minute;
+	return 0;
+}
