de681739ce85e529468500a132fb8e691c385db5
codingksj <45532564+codingksj@users.noreply.github.com>
2023-08-03 19:48:11 +0900

    Add 10189.cpp

---
+++ 백준/10000~10999/10100~10199/10189.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<set>
+#include<map>
+#include<vector>
+#include<queue>
+#include<deque>
+#include<algorithm>
+#include<string>
+#include<numeric>
+#include<cmath>
+#include<iomanip>
+
+using namespace std;
+
+typedef long long LL;
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	cout 
+		<< "#  # #### #### #  #\n" 
+		<< "#### #  # #  # # #\n"
+		<< "#### #  # #  # # #\n"
+		<< "#  # #### #### #  #\n";
+	return 0;
+}
