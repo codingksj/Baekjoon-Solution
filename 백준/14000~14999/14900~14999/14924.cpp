1554af1e304bba1ed04ac964785d4409342cfeb9
codingksj <45532564+codingksj@users.noreply.github.com>
2023-08-03 19:48:44 +0900

    Add 14924.cpp

---
+++ 백준/14000~14999/14900~14999/14924.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<set>
+#include<unordered_map>
+#include<map>
+#include<vector>
+#include<queue>
+#include<deque>
+#include<stack>
+#include<algorithm>
+#include<string>
+#include<numeric>
+#include<cmath>
+#include<cctype>
+#include<cstring>
+#include<iomanip>
+#include<chrono>
+
+using namespace std;
+
+typedef long long LL;
+typedef pair<int, int> Pii;
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	LL time, speedT, speedF, distance;
+	LL movement;
+
+	cin >> speedT >> speedF >> distance;
+
+	time = distance / (2LL * speedT);
+	movement = time * speedF;
+
+	cout << movement;
+
+	return 0;
+}
