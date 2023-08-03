acf54e232129ed58fe1721233019d786bf496737
codingksj <45532564+codingksj@users.noreply.github.com>
2023-08-03 19:47:54 +0900

    Add 08545.cpp

---
+++ 백준/08000~08999/08500~08599/08545.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<set>
+#include<map>
+#include<vector>
+#include<queue>
+#include<deque>
+#include<algorithm>
+#include<string>
+#include<numeric>
+#include<cmath>
+#include<iomanip>
+
+using namespace std;
+
+typedef long long LL;
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	string S;
+	cin >> S;
+	reverse(S.begin(), S.end());
+	cout << S;
+	return 0;
+}
