2c9bfa3c8b00a9ad6190692c3a6dd7a2f275eb30
codingksj <45532564+codingksj@users.noreply.github.com>
2023-08-03 19:49:05 +0900

    Add 17903.cpp

---
+++ 백준/17000~17999/17900~17999/17903.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<set>
+#include<unordered_map>
+#include<map>
+#include<vector>
+#include<queue>
+#include<deque>
+#include<stack>
+#include<algorithm>
+#include<string>
+#include<numeric>
+#include<cmath>
+#include<cctype>
+#include<cstring>
+#include<iomanip>
+#include<chrono>
+
+using namespace std;
+
+typedef long long LL;
+typedef pair<int, int> Pii;
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	const int SATISFACTORY = 8;
+	int m, n;
+	cin >> m >> n;
+
+	cout << (m >= SATISFACTORY ? "satisfactory\n" : "unsatisfactory\n");
+	return 0;
+}
