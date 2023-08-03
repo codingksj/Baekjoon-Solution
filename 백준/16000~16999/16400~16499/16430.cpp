8e237a08a78699303f0721007503c94f1802954a
codingksj <45532564+codingksj@users.noreply.github.com>
2023-08-03 19:48:58 +0900

    Add 16430.cpp

---
+++ 백준/16000~16999/16400~16499/16430.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<set>
+#include<map>
+#include<vector>
+#include<queue>
+#include<deque>
+#include<algorithm>
+#include<string>
+#include<numeric>
+#include<cmath>
+#include<cctype>
+#include<iomanip>
+
+using namespace std;
+
+typedef long long LL;
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	int A, B;
+	cin >> A >> B;
+	cout << B - A << " " << B;
+	return 0;
+}
