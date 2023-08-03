c743bf0577fb9c0bf617e9d4e5c93d2239ad5198
codingksj <45532564+codingksj@users.noreply.github.com>
2023-08-03 19:48:28 +0900

    Add 10988.cpp

---
+++ 백준/10000~10999/10900~10999/10988.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<set>
+#include<unordered_map>
+#include<map>
+#include<vector>
+#include<queue>
+#include<deque>
+#include<stack>
+#include<algorithm>
+#include<string>
+#include<numeric>
+#include<cmath>
+#include<cctype>
+#include<cstring>
+#include<iomanip>
+#include<chrono>
+
+using namespace std;
+
+typedef long long LL;
+typedef pair<int, int> Pii;
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	string S, revS;
+	cin >> S;
+	revS = S;
+	reverse(revS.begin(), revS.end());
+	cout << (S == revS);
+	return 0;
+}
