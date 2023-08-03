887cd593cf486faf88a863d8c0361b0411655828
codingksj <45532564+codingksj@users.noreply.github.com>
2023-08-03 19:49:13 +0900

    Add 20215.cpp

---
+++ 백준/20000~20999/20200~20299/20215.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<set>
+#include<unordered_map>
+#include<map>
+#include<vector>
+#include<queue>
+#include<deque>
+#include<stack>
+#include<algorithm>
+#include<string>
+#include<numeric>
+#include<cmath>
+#include<cctype>
+#include<cstring>
+#include<iomanip>
+#include<chrono>
+
+using namespace std;
+
+typedef long long LL;
+typedef pair<int, int> Pii;
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	int w, h;
+	double rect, diag;
+	cin >> w >> h;
+	rect = w + h;
+	diag = sqrt(w * w + h * h);
+	cout.precision(12);
+	cout << rect - diag;
+	return 0;
+}
