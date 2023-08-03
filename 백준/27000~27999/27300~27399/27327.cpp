234a685e982e57d9e4c7b1864cb5a4f92a82f4d2
codingksj <45532564+codingksj@users.noreply.github.com>
2023-08-03 19:49:29 +0900

    Add 27327.cpp

---
+++ 백준/27000~27999/27300~27399/27327.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<set>
+#include<map>
+#include<vector>
+#include<queue>
+#include<deque>
+#include<algorithm>
+#include<string>
+#include<numeric>
+#include<cmath>
+#include<cctype>
+#include<cstring>
+#include<iomanip>
+
+using namespace std;
+
+typedef long long LL;
+typedef pair<int, int> Pii;
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	int day;
+	cin >> day;
+	cout << 24 * day;
+	return 0;
+}
