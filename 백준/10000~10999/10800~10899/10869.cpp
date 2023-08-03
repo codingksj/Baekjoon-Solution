b3b1f4459a4a0b058d921eaeefd05bf6377587e7
codingksj <45532564+codingksj@users.noreply.github.com>
2023-08-03 19:48:24 +0900

    Add 10869.cpp

---
+++ 백준/10000~10999/10800~10899/10869.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<set>
+#include<unordered_map>
+#include<map>
+#include<vector>
+#include<queue>
+#include<deque>
+#include<stack>
+#include<algorithm>
+#include<string>
+#include<numeric>
+#include<cmath>
+#include<cctype>
+#include<cstring>
+#include<iomanip>
+#include<chrono>
+
+using namespace std;
+
+typedef long long LL;
+typedef pair<int, int> Pii;
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	int a, b;
+	cin >> a >> b;
+	cout << a + b << "\n"
+		<< a - b << "\n"
+		<< a * b << "\n"
+		<< a / b << "\n"
+		<< a % b << "\n";
+
+	return 0;
+}
