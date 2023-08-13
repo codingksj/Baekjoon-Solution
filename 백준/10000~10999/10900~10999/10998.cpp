c472d83039760552df5093ce7ed4e6f9e69e1716
codingksj <45532564+codingksj@users.noreply.github.com>
2023-08-13 22:25:00 +0900

    Add 10998.cpp

---
+++ 백준/10000~10999/10900~10999/10998.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<set>
+#include<unordered_map>
+#include<map>
+#include<vector>
+#include<queue>
+#include<deque>
+#include<stack>
+#include<algorithm>
+#include<string>
+#include<numeric>
+#include<cmath>
+#include<cctype>
+#include<cstring>
+#include<iomanip>
+#include<chrono>
+
+using namespace std;
+
+typedef long long LL;
+typedef pair<int, int> Pii;
+typedef pair<LL, LL> Pll;
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	int A, B;
+	cin >> A >> B;
+	cout << A * B;
+	return 0;
+}
+
