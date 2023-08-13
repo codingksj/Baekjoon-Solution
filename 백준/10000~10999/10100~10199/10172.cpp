b8939edf82c41d6e5cadc5f6ee043ac56365de93
codingksj <45532564+codingksj@users.noreply.github.com>
2023-08-13 22:24:46 +0900

    Add 10172.cpp

---
+++ 백준/10000~10999/10100~10199/10172.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<set>
+#include<unordered_map>
+#include<map>
+#include<vector>
+#include<queue>
+#include<deque>
+#include<stack>
+#include<algorithm>
+#include<string>
+#include<numeric>
+#include<cmath>
+#include<cctype>
+#include<cstring>
+#include<iomanip>
+#include<chrono>
+
+using namespace std;
+
+typedef long long LL;
+typedef pair<int, int> Pii;
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+    cout << "|\\_/|\n";
+    cout << "|q p|   /}\n";
+    cout << "( 0 )\"\"\"\\\n";
+    cout << "|\"^\"`    |\n";
+    cout << "||_/=\\\\__|\n";
+	return 0;
+}
