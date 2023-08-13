9ffaf89121d55ff8457e91d855e9b1cb60f8cbec
codingksj <45532564+codingksj@users.noreply.github.com>
2023-08-13 22:24:23 +0900

    Add 05666.cpp

---
+++ 백준/05000~05999/05600~05699/05666.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<set>
+#include<unordered_map>
+#include<map>
+#include<vector>
+#include<queue>
+#include<deque>
+#include<stack>
+#include<algorithm>
+#include<string>
+#include<numeric>
+#include<cmath>
+#include<cctype>
+#include<cstring>
+#include<iomanip>
+#include<chrono>
+
+using namespace std;
+
+typedef long long LL;
+typedef pair<int, int> Pii;
+typedef pair<LL, LL> Pll;
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	int H, P;
+	double avg;
+
+	while (cin >> H >> P) {
+		cout << fixed;
+		cout.precision(2);
+		avg = (double)H / P;
+		cout << avg << "\n";
+	}
+
+	return 0;
+}
