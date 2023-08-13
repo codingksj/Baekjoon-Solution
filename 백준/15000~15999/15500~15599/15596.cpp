2787053a50b2d1d1d35979cc25894dfbe001b4d1
codingksj <45532564+codingksj@users.noreply.github.com>
2023-08-13 22:25:34 +0900

    Add 15596.cpp

---
+++ 백준/15000~15999/15500~15599/15596.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<bitset>
+#include<set>
+#include<unordered_map>
+#include<map>
+#include<vector>
+#include<queue>
+#include<deque>
+#include<stack>
+#include<algorithm>
+#include<string>
+#include<numeric>
+#include<cmath>
+#include<cctype>
+#include<cstring>
+#include<iomanip>
+#include<chrono>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef long double LD;
+typedef pair<int, int> Pii;
+typedef pair<LL, LL> Pll;
+
+long sum(vector<int>& a) {
+	long total = 0;
+	for (auto e : a) {
+		total += e;
+	}
+	return total;
+};
