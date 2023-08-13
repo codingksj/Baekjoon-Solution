ce1ba9efbfe9bf2af04a83c36b078614d13bcee0
codingksj <45532564+codingksj@users.noreply.github.com>
2023-08-13 22:25:28 +0900

    Add 14681.cpp

---
+++ 백준/14000~14999/14600~14699/14681.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<set>
+#include<unordered_map>
+#include<map>
+#include<vector>
+#include<queue>
+#include<deque>
+#include<stack>
+#include<algorithm>
+#include<string>
+#include<numeric>
+#include<cmath>
+#include<cctype>
+#include<cstring>
+#include<iomanip>
+#include<chrono>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef long double LD;
+typedef pair<int, int> Pii;
+typedef pair<LL, LL> Pll;
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	int x, y;
+
+	cin >> x >> y;
+
+	cout << (x > 0 ? (y > 0 ? 1 : 4) : (y > 0 ? 2 : 3));
+
+	return 0;
+}
+
