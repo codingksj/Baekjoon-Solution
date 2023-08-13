325a07d0c6800b1219f7334dd0bc038b1cd98bcd
codingksj <45532564+codingksj@users.noreply.github.com>
2023-08-13 22:25:11 +0900

    Add 11654.cpp

---
+++ 백준/11000~11999/11600~11699/11654.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<set>
+#include<unordered_map>
+#include<map>
+#include<vector>
+#include<queue>
+#include<deque>
+#include<stack>
+#include<algorithm>
+#include<string>
+#include<numeric>
+#include<cmath>
+#include<cctype>
+#include<cstring>
+#include<iomanip>
+#include<chrono>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef pair<int, int> Pii;
+typedef pair<LL, LL> Pll;
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	char c;
+	int ascii;
+
+	cin >> c;
+	ascii = c;
+	cout << ascii;
+
+	return 0;
+}
