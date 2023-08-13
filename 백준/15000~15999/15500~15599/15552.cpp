8c2c886ca0a23070f94eccb4d6d461565f0b17e7
codingksj <45532564+codingksj@users.noreply.github.com>
2023-08-13 22:25:34 +0900

    Add 15552.cpp

---
+++ 백준/15000~15999/15500~15599/15552.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<bitset>
+#include<set>
+#include<unordered_map>
+#include<map>
+#include<vector>
+#include<queue>
+#include<deque>
+#include<stack>
+#include<algorithm>
+#include<string>
+#include<numeric>
+#include<cmath>
+#include<cctype>
+#include<cstring>
+#include<iomanip>
+#include<chrono>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef long double LD;
+typedef pair<int, int> Pii;
+typedef pair<LL, LL> Pll;
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	int TC;
+	int A, B;
+
+	cin >> TC;
+
+	while (TC--) {
+		cin >> A >> B;
+		cout << A + B << "\n";
+	}
+
+	return 0;
+}
