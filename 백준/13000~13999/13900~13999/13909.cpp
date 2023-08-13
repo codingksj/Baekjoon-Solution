c62be130cc742d6922749a660d9fa696669237d1
codingksj <45532564+codingksj@users.noreply.github.com>
2023-08-13 22:25:23 +0900

    Add 13909.cpp

---
+++ 백준/13000~13999/13900~13999/13909.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<set>
+#include<unordered_map>
+#include<map>
+#include<vector>
+#include<queue>
+#include<deque>
+#include<stack>
+#include<algorithm>
+#include<string>
+#include<numeric>
+#include<cmath>
+#include<cctype>
+#include<cstring>
+#include<iomanip>
+#include<chrono>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef long double LD;
+typedef pair<int, int> Pii;
+typedef pair<LL, LL> Pll;
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	LL N, count;
+	
+	cin >> N;
+
+	count = floor(sqrt(N));
+
+	cout << count;
+
+	return 0;
+}
