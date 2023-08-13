cc6724f7c224c35b43e25e9cc4dab9a36e706a5e
codingksj <45532564+codingksj@users.noreply.github.com>
2023-08-13 22:25:41 +0900

    Add 15894.cpp

---
+++ 백준/15000~15999/15800~15899/15894.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<sstream>
+#include<bitset>
+#include<set>
+#include<unordered_map>
+#include<map>
+#include<vector>
+#include<queue>
+#include<deque>
+#include<stack>
+#include<algorithm>
+#include<string>
+#include<numeric>
+#include<cmath>
+#include<cctype>
+#include<cstring>
+#include<iomanip>
+#include<chrono>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef long double LD;
+typedef pair<int, int> Pii;
+typedef pair<LL, LL> PLL;
+
+int main() {
+	LL N;
+
+	cin >> N;
+	cout << 4 * N;
+
+	return 0;
+}
