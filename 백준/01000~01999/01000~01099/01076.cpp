328ec40bc4e02b5bc02cb532a1c6ad43ede2d8d9
codingksj <45532564+codingksj@users.noreply.github.com>
2023-08-14 21:43:55 +0900

    Update 01076.cpp

--- 백준/01000~01999/01000~01099/01076.cpp
+++ 백준/01000~01999/01000~01099/01076.cpp
@@ -26,43 +26,43 @@ int main() {
 	cin.tie(NULL);
 	ios::sync_with_stdio(false);
 
-    map<string, LL> color = {
-        {"black", 0},
-        {"brown", 1},
-        {"red", 2},
-        {"orange", 3},
-        {"yellow", 4},
-        {"green", 5},
-        {"blue", 6},
-        {"violet", 7},
-        {"grey", 8},
-        {"white", 9}
-    };
+	map<string, LL> color = {
+        	{"black", 0},
+        	{"brown", 1},
+        	{"red", 2},
+        	{"orange", 3},
+        	{"yellow", 4},
+        	{"green", 5},
+        	{"blue", 6},
+        	{"violet", 7},
+        	{"grey", 8},
+        	{"white", 9}
+    	};
 
-    map<string, LL> weight = {
-        {"black", 1E+0},
-        {"brown", 1E+1},
-        {"red", 1E+2},
-        {"orange", 1E+3},
-        {"yellow", 1E+4},
-        {"green", 1E+5},
-        {"blue", 1E+6},
-        {"violet", 1E+7},
-        {"grey", 1E+8},
-        {"white", 1E+9}
-    };
+	map<string, LL> weight = {
+	        {"black", 1E+0},
+	        {"brown", 1E+1},
+	        {"red", 1E+2},
+	        {"orange", 1E+3},
+	        {"yellow", 1E+4},
+	        {"green", 1E+5},
+	        {"blue", 1E+6},
+	        {"violet", 1E+7},
+	        {"grey", 1E+8},
+	        {"white", 1E+9}
+	};
 
 
 	string R1, R2, R3;
-    LL ohm;
+    	LL ohm;
 
 	cin >> R1;
 	cin >> R2;
 	cin >> R3;
 
-    ohm = (color[R1] * 10LL + color[R2]) * weight[R3];
+    	ohm = (color[R1] * 10LL + color[R2]) * weight[R3];
 
-    cout << ohm;
+    	cout << ohm;
 
 	return 0;
 }
