7e6dfd5544fe112ccb51e205c503175404f0f739
codingksj <45532564+codingksj@users.noreply.github.com>
2023-08-18 23:04:08 +0900

    Add 02163.cpp

---
+++ 백준/02000~02999/02100~02199/02163.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<sstream>
+#include<bitset>
+#include<set>
+#include<unordered_map>
+#include<map>
+#include<vector>
+#include<queue>
+#include<deque>
+#include<stack>
+#include<algorithm>
+#include<string>
+#include<numeric>
+#include<cmath>
+#include<cctype>
+#include<cstring>
+#include<iomanip>
+#include<chrono>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef long double LD;
+typedef pair<int, int> Pii;
+typedef pair<LL, LL> PLL;
+typedef pair<string, int> Psi;
+typedef map<string, string> MapSS;
+typedef map<string, LL> MapSL;
+typedef unordered_map<string, string> UMapSS;
+typedef vector<string> Words;
+
+int main() {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    int N, M;
+
+    cin >> N >> M;
+
+    cout << N * M - 1;
+
+    return 0;
+}
