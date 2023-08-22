cef2b8e2428ec8f3dc70e3e7851898b8f8712381
codingksj <45532564+codingksj@users.noreply.github.com>
2023-08-22 23:25:19 +0900

    Add 04500.cpp

---
+++ 백준/04000~04999/04500~04599/04500.cpp
@@ -0,0 +1,92 @@
+#include<iostream>
+#include<sstream>
+#include<bitset>
+#include<set>
+#include<unordered_map>
+#include<map>
+#include<vector>
+#include<queue>
+#include<deque>
+#include<stack>
+#include<algorithm>
+#include<string>
+#include<numeric>
+#include<cmath>
+#include<cctype>
+#include<cstring>
+#include<iomanip>
+#include<chrono>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef long double LD;
+typedef pair<int, bool> Pib;
+typedef pair<int, int> Pii;
+typedef pair<int, string> Pis;
+typedef pair<double, double> Pdd;
+typedef pair<LL, LL> PLL;
+typedef pair<string, int> Psi;
+typedef map<int, string> MapiS;
+typedef map<string, char> MapSc;
+typedef map<string, int> MapSi;
+typedef map<string, string> MapSS;
+typedef map<string, LL> MapSL;
+typedef unordered_map<string, LL> HashSL;
+typedef unordered_map<string, double> HashSd;
+typedef unordered_map<string, string> HashSS;
+typedef vector<string> Words;
+typedef vector<vector<int>> Matrix2Di;
+
+string FindWinner(Words& names, string name, int N);
+
+int main() {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    Words names;
+    string name, line, winner;
+    int TC, N;
+
+    cin >> TC;
+    cin.ignore();
+
+    while (TC--) {
+        getline(cin, line);
+
+        istringstream iss(line);
+
+        while (iss >> name) {
+            names.push_back(name);
+        }
+
+        cin >> name;
+        cin >> N;
+
+        winner = FindWinner(names, name, N);
+        cout << winner << "\n";
+
+        names.clear();
+        cin.ignore();
+    }
+
+    return 0;
+}
+
+string FindWinner(Words& names, string name, int N) {
+    int start = 0;
+    int size = names.size();
+    int index;
+
+    for (int i = 0; i < size; i++) {
+        if (names[i] == name) {
+            start = i;
+            break;
+        }
+    }
+
+    index = (start + N - 1) % size;
+
+    return names[index];
+}
