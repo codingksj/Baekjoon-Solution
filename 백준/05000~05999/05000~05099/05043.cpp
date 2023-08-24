87d3be02d40d5a2220566931c4176cca576c344c
codingksj <45532564+codingksj@users.noreply.github.com>
2023-08-24 17:09:45 +0900

    Add 05043.cpp

---
+++ 백준/05000~05999/05000~05099/05043.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<sstream>
+#include<bitset>
+#include<set>
+#include<unordered_map>
+#include<map>
+#include<vector>
+#include<queue>
+#include<deque>
+#include<stack>
+#include<algorithm>
+#include<string>
+#include<numeric>
+#include<cmath>
+#include<cctype>
+#include<cstring>
+#include<iomanip>
+#include<chrono>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef long double LD;
+typedef pair<int, bool> Pib;
+typedef pair<int, int> Pii;
+typedef pair<int, string> Pis;
+typedef pair<double, double> Pdd;
+typedef pair<LL, LL> PLL;
+typedef pair<string, int> Psi;
+typedef map<int, string> MapiS;
+typedef map<string, char> MapSc;
+typedef map<string, int> MapSi;
+typedef map<string, string> MapSS;
+typedef map<string, LL> MapSL;
+typedef unordered_map<string, LL> HashSL;
+typedef unordered_map<string, double> HashSd;
+typedef unordered_map<string, string> HashSS;
+typedef vector<string> Words;
+typedef vector<vector<int>> Matrix2D_i;
+
+#define PI 3.141592653589793
+
+int main() {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    ULL N, b;
+    ULL possibleMax;
+
+    cin >> N >> b;
+
+    possibleMax = (1ULL << (b + 1)) - 1;
+
+    cout << (N <= possibleMax ? "yes" : "no") << "\n";
+
+    return 0;
+}
