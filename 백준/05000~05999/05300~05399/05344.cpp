a2a72fbf73dd621368c66e22aae2fbe7b7478d94
codingksj <45532564+codingksj@users.noreply.github.com>
2023-08-25 23:41:44 +0900

    Add 05344.cpp

---
+++ 백준/05000~05999/05300~05399/05344.cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+#include<sstream>
+#include<bitset>
+#include<set>
+#include<unordered_map>
+#include<map>
+#include<vector>
+#include<queue>
+#include<deque>
+#include<stack>
+#include<tuple>
+#include<algorithm>
+#include<string>
+#include<numeric>
+#include<cmath>
+#include<cctype>
+#include<cstring>
+#include<iomanip>
+#include<chrono>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef long double LD;
+
+typedef pair<int, bool> Pib;
+typedef pair<int, int> Pii;
+typedef pair<int, string> PiS;
+typedef pair<double, double> Pdd;
+typedef pair<LL, LL> PLL;
+typedef pair<string, int> PSi;
+typedef pair<string, LL> PSL;
+
+typedef map<int, string> MapiS;
+typedef map<string, char> MapSc;
+typedef map<string, int> MapSi;
+typedef map<string, string> MapSS;
+typedef map<string, LL> MapSL;
+
+typedef unordered_map<string, LL> HashSL;
+typedef unordered_map<string, double> HashSd;
+typedef unordered_map<string, string> HashSS;
+
+typedef vector<string> Words;
+typedef vector<vector<int>> Matrix2D_i;
+
+LL GCD(LL a, LL b) {
+    if (b == 0) {
+        return a;
+    }
+    return GCD(b, a % b);
+}
+
+int main() {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    LL a, b, gcd;
+    int TC;
+
+    cin >> TC;
+
+    while (TC--) {
+        cin >> a >> b;
+
+        gcd = GCD(a, b);
+
+        cout << gcd << "\n";
+    }
+
+    return 0;
+};
