bfb32afa9ffe0c0ea065d99bf6996d94773adda8
codingksj <45532564+codingksj@users.noreply.github.com>
2023-08-26 20:26:37 +0900

    Add 01094.cpp

---
+++ 백준/01000~01999/01000~01099/01094.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+#include<sstream>
+#include<bitset>
+#include<set>
+#include<unordered_map>
+#include<map>
+#include<vector>
+#include<queue>
+#include<deque>
+#include<stack>
+#include<tuple>
+#include<algorithm>
+#include<string>
+#include<numeric>
+#include<cmath>
+#include<cctype>
+#include<cstring>
+#include<iomanip>
+#include<chrono>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef long double LD;
+
+typedef pair<int, bool> Pib;
+typedef pair<int, int> Pii;
+typedef pair<int, string> PiS;
+typedef pair<double, double> Pdd;
+typedef pair<LL, LL> PLL;
+typedef pair<string, int> PSi;
+typedef pair<string, LL> PSL;
+
+typedef map<int, string> MapiS;
+typedef map<string, char> MapSc;
+typedef map<string, int> MapSi;
+typedef map<string, string> MapSS;
+typedef map<string, LL> MapSL;
+
+typedef unordered_map<string, LL> HashSL;
+typedef unordered_map<string, double> HashSd;
+typedef unordered_map<string, string> HashSS;
+
+typedef vector<string> Words;
+typedef vector<vector<int>> Matrix2D_i;
+
+int main() {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    int N;
+
+    cin >> N;
+
+    cout << bitset<8>(N).count() << "\n";
+
+    return 0;
+};
