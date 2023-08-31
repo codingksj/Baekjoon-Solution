1e8a5fa1749ac413ec15e601a16e9385e1b4c3ac
codingksj <45532564+codingksj@users.noreply.github.com>
2023-08-31 22:56:01 +0900

    Add 03553.cpp

---
+++ 백준/03000~03999/03500~03599/03553.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+#include<sstream>
+#include<bitset>
+#include<set>
+#include<unordered_map>
+#include<map>
+#include<vector>
+#include<queue>
+#include<deque>
+#include<stack>
+#include<tuple>
+#include<algorithm>
+#include<string>
+#include<numeric>
+#include<cmath>
+#include<cctype>
+#include<cstring>
+#include<iomanip>
+#include<chrono>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef long double LD;
+
+typedef pair<int, bool> Pib;
+typedef pair<int, int> Pii;
+typedef pair<int, string> PiS;
+typedef pair<double, double> Pdd;
+typedef pair<LL, LL> PLL;
+typedef pair<string, int> PSi;
+typedef pair<string, LL> PSL;
+
+typedef map<int, string> MapiS;
+typedef map<string, char> MapSc;
+typedef map<string, int> MapSi;
+typedef map<string, string> MapSS;
+typedef map<string, LL> MapSL;
+
+typedef unordered_map<string, LL> HashSL;
+typedef unordered_map<string, double> HashSd;
+typedef unordered_map<string, string> HashSS;
+
+typedef vector<string> Words;
+typedef vector<vector<int>> Matrix2D_i;
+
+int main() {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    int N, D;
+    int dLength;
+
+    cin >> N >> D;
+
+    dLength = to_string(D).length();
+
+    N < dLength ? 
+        cout << "No solution" << "\n" :
+        cout << D << string(N - dLength, '0') << "\n";
+
+    return 0;
+}
+
