5d0dce84ed1b18837ae0f28facc07ab2b332ca06
codingksj <45532564+codingksj@users.noreply.github.com>
2023-09-05 21:46:25 +0900

    Add 05046.cpp

---
+++ 백준/05000~05999/05000~05099/05046.cpp
@@ -0,0 +1,79 @@
+#include<iostream>
+#include<sstream>
+#include<bitset>
+#include<set>
+#include<unordered_map>
+#include<map>
+#include<vector>
+#include<queue>
+#include<deque>
+#include<stack>
+#include<tuple>
+#include<algorithm>
+#include<string>
+#include<numeric>
+#include<cmath>
+#include<cctype>
+#include<cstring>
+#include<iomanip>
+#include<chrono>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef long double LD;
+
+typedef pair<char, char> Pcc;
+typedef pair<char, int> Pci;
+typedef pair<char, string> PcS;
+typedef pair<int, bool> Pib;
+typedef pair<int, int> Pii;
+typedef pair<int, string> PiS;
+typedef pair<double, double> Pdd;
+typedef pair<LL, LL> PLL;
+typedef pair<string, int> PSi;
+typedef pair<string, LL> PSL;
+typedef pair<string, string> PSS;
+
+typedef map<int, string> MapiS;
+typedef map<string, char> MapSc;
+typedef map<string, int> MapSi;
+typedef map<string, string> MapSS;
+typedef map<string, LL> MapSL;
+
+typedef unordered_map<string, LL> HashSL;
+typedef unordered_map<string, double> HashSd;
+typedef unordered_map<string, string> HashSS;
+
+typedef vector<string> Words;
+typedef vector<vector<int>> Matrix2D_i;
+typedef vector<vector<LL>> Matrix2D_L;
+
+int main() {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    
+    LL participants, budget, hotels, weeks;
+    LL price, available;
+    LL minPrice;
+
+    cin >> participants >> budget >> hotels >> weeks;
+
+    minPrice = budget + 1;
+
+    while (hotels--) {
+        cin >> price;
+
+        for (int week = 0; week < weeks; week++) {
+            cin >> available;
+            if (available >= participants) {
+                minPrice = min(minPrice, price * participants);
+            }
+        }
+    }
+
+    cout << (minPrice > budget ? "stay home" : to_string(minPrice)) << "\n";
+
+    return 0;
+}
