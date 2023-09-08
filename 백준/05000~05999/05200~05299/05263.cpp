92993fedd486bf17251c1b6d14e4739fea0c480f
codingksj <45532564+codingksj@users.noreply.github.com>
2023-09-08 20:21:32 +0900

    Add 05263.cpp

---
+++ 백준/05000~05999/05200~05299/05263.cpp
@@ -0,0 +1,77 @@
+#include<iostream>
+#include<sstream>
+#include<bitset>
+#include<set>
+#include<unordered_map>
+#include<map>
+#include<vector>
+#include<queue>
+#include<deque>
+#include<stack>
+#include<tuple>
+#include<algorithm>
+#include<string>
+#include<numeric>
+#include<cmath>
+#include<cctype>
+#include<cstring>
+#include<iomanip>
+#include<chrono>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef long double LD;
+
+typedef pair<char, char> Pcc;
+typedef pair<char, int> Pci;
+typedef pair<char, string> PcS;
+typedef pair<int, bool> Pib;
+typedef pair<int, int> Pii;
+typedef pair<int, string> PiS;
+typedef pair<double, double> Pdd;
+typedef pair<LL, LL> PLL;
+typedef pair<string, int> PSi;
+typedef pair<string, LL> PSL;
+typedef pair<string, string> PSS;
+
+typedef map<int, string> MapiS;
+typedef map<LL, LL> MapLL;
+typedef map<string, char> MapSc;
+typedef map<string, int> MapSi;
+typedef map<string, string> MapSS;
+typedef map<string, LL> MapSL;
+
+typedef unordered_map<string, LL> HashSL;
+typedef unordered_map<string, double> HashSd;
+typedef unordered_map<string, string> HashSS;
+
+typedef vector<string> Words;
+typedef vector<vector<int>> Matrix2D_i;
+typedef vector<vector<LL>> Matrix2D_L;
+
+int main() {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    
+    MapLL schools;
+    int n, k;
+    int id;
+
+    cin >> n >> k;
+
+    for (int i = 0; i < n; i++) {
+        cin >> id;
+        schools[id]++;
+    }
+
+    for (PLL p : schools) {
+        if (p.second % k) {
+            cout << p.first;
+            break;
+        }
+    }
+
+    return 0;
+}
