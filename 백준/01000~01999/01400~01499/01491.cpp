e9da37d738feca233db98e5cea0c26781f3d7b54
codingksj <45532564+codingksj@users.noreply.github.com>
2023-09-16 10:41:07 +0900

    Add 01491.cpp

---
+++ 백준/01000~01999/01400~01499/01491.cpp
@@ -0,0 +1,84 @@
+#include<iostream>
+#include<sstream>
+#include<bitset>
+#include<set>
+#include<unordered_map>
+#include<map>
+#include<vector>
+#include<queue>
+#include<deque>
+#include<stack>
+#include<tuple>
+#include<algorithm>
+#include<string>
+#include<numeric>
+#include<cmath>
+#include<cctype>
+#include<cstring>
+#include<iomanip>
+#include<chrono>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef long double LD;
+
+typedef pair<char, char> Pcc;
+typedef pair<char, int> Pci;
+typedef pair<char, string> PcS;
+typedef pair<int, bool> Pib;
+typedef pair<int, int> Pii;
+typedef pair<int, string> PiS;
+typedef pair<double, double> Pdd;
+typedef pair<LL, LL> PLL;
+typedef pair<string, int> PSi;
+typedef pair<string, LL> PSL;
+typedef pair<string, string> PSS;
+
+typedef map<int, string> MapiS;
+typedef map<LL, LL> MapLL;
+typedef map<string, char> MapSc;
+typedef map<string, int> MapSi;
+typedef map<string, string> MapSS;
+typedef map<string, LL> MapSL;
+
+typedef unordered_map<string, LL> HashSL;
+typedef unordered_map<string, double> HashSd;
+typedef unordered_map<string, string> HashSS;
+
+typedef vector<string> Words;
+typedef vector<vector<int>> Matrix2D_i;
+typedef vector<vector<LL>> Matrix2D_L;
+
+Pii FIndSpiralEndCoord(const int N, const int M);
+
+int main() {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    Pii coord;
+    int N, M;
+
+    cin >> N >> M;
+
+    coord = FIndSpiralEndCoord(N, M);
+
+    cout << coord.first << " " << coord.second << "\n";
+
+    return 0;
+}
+
+Pii FIndSpiralEndCoord(const int N, const int M) {
+    Pii coord;
+    Pii p1 = { N - 1 - M / 2, M / 2 };
+    Pii p2 = { M / 2 - 1, M / 2 };
+    Pii p3 = { N / 2, M - 1 - N / 2 };
+    Pii p4 = { N / 2 - 1, N / 2 };
+
+    coord = (N >= M) ?
+        ((M & 1) ? p1 : p2) :
+        ((N & 1) ? p3 : p4);
+
+    return coord;
+};
