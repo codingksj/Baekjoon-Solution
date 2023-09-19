cd231b90c23e4994530ce2d59cab4eb35a9c2ae6
codingksj <45532564+codingksj@users.noreply.github.com>
2023-09-19 22:03:39 +0900

    Update and rename 10814.c to 10814.cpp

---
+++ 백준/10000~10999/10800~10899/10814.cpp
@@ -0,0 +1,90 @@
+#include<iostream>
+#include<sstream>
+#include<bitset>
+#include<set>
+#include<unordered_map>
+#include<map>
+#include<vector>
+#include<queue>
+#include<deque>
+#include<stack>
+#include<tuple>
+#include<algorithm>
+#include<string>
+#include<numeric>
+#include<cmath>
+#include<cctype>
+#include<cstring>
+#include<iomanip>
+#include<chrono>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef long double LD;
+
+typedef pair<char, char> Pcc;
+typedef pair<char, int> Pci;
+typedef pair<char, string> PcS;
+typedef pair<int, bool> Pib;
+typedef pair<int, int> Pii;
+typedef pair<int, string> PiS;
+typedef pair<double, double> Pdd;
+typedef pair<LL, LL> PLL;
+typedef pair<string, int> PSi;
+typedef pair<string, LL> PSL;
+typedef pair<string, string> PSS;
+
+typedef map<int, string> MapiS;
+typedef map<LL, LL> MapLL;
+typedef map<string, char> MapSc;
+typedef map<string, int> MapSi;
+typedef map<string, string> MapSS;
+typedef map<string, LL> MapSL;
+
+typedef unordered_map<string, LL> HashSL;
+typedef unordered_map<string, double> HashSd;
+typedef unordered_map<string, string> HashSS;
+
+typedef vector<string> Words;
+typedef vector<vector<int>> Matrix2D_i;
+typedef vector<vector<LL>> Matrix2D_L;
+
+typedef struct Member {
+    string name;
+    int index;
+    int age;
+    
+    bool operator< (const Member& other) {
+        if (age != other.age) {
+            return age < other.age;
+        }
+        return index < other.index;
+    }
+}Member;
+
+int main() {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    vector<Member> members;
+    int N;
+
+    cin >> N;
+
+    members.resize(N);
+
+    for (int i = 0; i < N; i++) {
+        cin >> members[i].age >> members[i].name;
+        members[i].index = i;
+    }
+
+    sort(members.begin(), members.end());
+
+    for (const Member& member : members) {
+        cout << member.age << " " << member.name << "\n";
+    }
+
+    return 0;
+}
