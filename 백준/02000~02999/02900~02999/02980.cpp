9de0b5ee117fb393bb606f5a1f63431afef68d93
codingksj <45532564+codingksj@users.noreply.github.com>
2023-09-23 14:56:41 +0900

    Add 02980.cpp

---
+++ 백준/02000~02999/02900~02999/02980.cpp
@@ -0,0 +1,109 @@
+#include<iostream>
+#include<sstream>
+#include<bitset>
+#include<set>
+#include<unordered_map>
+#include<map>
+#include<vector>
+#include<queue>
+#include<deque>
+#include<stack>
+#include<tuple>
+#include<algorithm>
+#include<string>
+#include<numeric>
+#include<cmath>
+#include<cctype>
+#include<cstring>
+#include<iomanip>
+#include<chrono>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef long double LD;
+
+typedef pair<char, char> Pcc;
+typedef pair<char, int> Pci;
+typedef pair<char, string> PcS;
+typedef pair<int, bool> Pib;
+typedef pair<int, int> Pii;
+typedef pair<int, string> PiS;
+typedef pair<double, double> Pdd;
+typedef pair<LL, LL> PLL;
+typedef pair<string, int> PSi;
+typedef pair<string, LL> PSL;
+typedef pair<string, string> PSS;
+
+typedef map<int, int> Mapii;
+typedef map<int, string> MapiS;
+typedef map<LL, LL> MapLL;
+typedef map<string, char> MapSc;
+typedef map<string, int> MapSi;
+typedef map<string, string> MapSS;
+typedef map<string, LL> MapSL;
+
+typedef unordered_map<string, LL> HashSL;
+typedef unordered_map<string, double> HashSd;
+typedef unordered_map<string, string> HashSS;
+
+typedef vector<string> Words;
+typedef vector<vector<int>> Matrix2D_i;
+typedef vector<vector<LL>> Matrix2D_L;
+typedef vector<vector<double>> Matrix2D_d;
+
+typedef struct TrafficLight {
+    int position;
+    int red;
+    int green;
+    int cycle;
+}TrafficLight;
+
+vector<TrafficLight> LoadTrafficLights(const int N);
+int CalcMovementTime(const vector<TrafficLight>& trafficLights, const int L);
+
+int main() {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    vector<TrafficLight> trafficLights;
+    int N, L;
+    int time;
+
+    cin >> N >> L;
+
+    trafficLights = LoadTrafficLights(N);
+    time = CalcMovementTime(trafficLights, L);
+
+    cout << time << "\n";
+
+    return 0;
+}
+
+vector<TrafficLight> LoadTrafficLights(const int N) {
+    vector<TrafficLight> trafficLights(N);
+
+    for (TrafficLight& light : trafficLights) {
+        cin >> light.position >> light.red >> light.green;
+        light.cycle = light.red + light.green;
+    }
+
+    return trafficLights;
+};
+
+int CalcMovementTime(const vector<TrafficLight>& trafficLights, const int L) {
+    int time = 0;
+    int remainTime;
+
+    for (int d = 0; d < L; d++, time++) {
+        for (const TrafficLight& light : trafficLights) {
+            remainTime = light.red - time % light.cycle;
+            if ((d == light.position) && (remainTime > 0)) {
+                time += remainTime;
+            }
+        }
+    }
+
+    return time;
+};
