5d8981746024d7ae5ef353cd2bbe40e09c5dea13
codingksj <45532564+codingksj@users.noreply.github.com>
2023-09-28 23:12:02 +0900

    Add 11652.cpp

---
+++ 백준/11000~11999/11600~11699/11652.cpp
@@ -0,0 +1,105 @@
+#include<iostream>
+#include<sstream>
+#include<bitset>
+#include<set>
+#include<unordered_map>
+#include<map>
+#include<vector>
+#include<queue>
+#include<deque>
+#include<stack>
+#include<tuple>
+#include<algorithm>
+#include<string>
+#include<numeric>
+#include<cmath>
+#include<cctype>
+#include<cstring>
+#include<iomanip>
+#include<chrono>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef long double LD;
+
+typedef pair<char, char> Pcc;
+typedef pair<char, int> Pci;
+typedef pair<char, string> PcS;
+typedef pair<int, bool> Pib;
+typedef pair<int, int> Pii;
+typedef pair<int, string> PiS;
+typedef pair<double, double> Pdd;
+typedef pair<LL, LL> PLL;
+typedef pair<string, int> PSi;
+typedef pair<string, LL> PSL;
+typedef pair<string, string> PSS;
+
+typedef map<int, int> Mapii;
+typedef map<int, string> MapiS;
+typedef map<LL, LL> MapLL;
+typedef map<string, char> MapSc;
+typedef map<string, int> MapSi;
+typedef map<string, string> MapSS;
+typedef map<string, LL> MapSL;
+
+typedef unordered_map<string, LL> HashSL;
+typedef unordered_map<string, double> HashSd;
+typedef unordered_map<string, string> HashSS;
+
+typedef vector<string> Words;
+typedef vector<vector<int>> Matrix2D_i;
+typedef vector<vector<LL>> Matrix2D_L;
+typedef vector<vector<double>> Matrix2D_d;
+
+vector<LL> LoadCards(const int N);
+LL FindMostFrequentCard(const vector<LL>& cards, const int N);
+
+int main() {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    vector<LL> cards;
+    LL ans;
+    int N;
+
+    cin >> N;
+
+    cards = LoadCards(N);
+
+    sort(cards.begin(), cards.end());
+
+    ans = FindMostFrequentCard(cards, N);
+
+    cout << ans << "\n";
+
+    return 0;
+}
+
+vector<LL> LoadCards(const int N) {
+    vector<LL> cards(N);
+    
+    for (LL& card : cards) {
+        cin >> card;
+    }
+
+    return cards;
+};
+
+LL FindMostFrequentCard(const vector<LL>& cards, const int N) {
+    LL ans = -1, number;
+    LL cnt, maxCnt = -1;
+
+    for (LL i = 0; i < N; i += cnt) {
+        number = cards[i];
+        cnt = upper_bound(cards.begin(), cards.end(), number) -
+            lower_bound(cards.begin(), cards.end(), number);
+        if (cnt > maxCnt) {
+            maxCnt = cnt;
+            ans = number;
+        }
+    }
+
+    return ans;
+};
