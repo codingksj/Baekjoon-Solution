7ae5b731c3165a2a8942e3687c583090c8ecb6d2
codingksj <45532564+codingksj@users.noreply.github.com>
2023-09-29 21:33:01 +0900

    Add 15624.cpp

---
+++ 백준/15000~15999/15600~15699/15624.cpp
@@ -0,0 +1,90 @@
+#include<iostream>
+#include<sstream>
+#include<bitset>
+#include<set>
+#include<unordered_map>
+#include<map>
+#include<vector>
+#include<queue>
+#include<deque>
+#include<stack>
+#include<tuple>
+#include<algorithm>
+#include<string>
+#include<numeric>
+#include<cmath>
+#include<cctype>
+#include<cstring>
+#include<iomanip>
+#include<chrono>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef long double LD;
+
+typedef pair<char, char> Pcc;
+typedef pair<char, int> Pci;
+typedef pair<char, string> PcS;
+typedef pair<int, bool> Pib;
+typedef pair<int, int> Pii;
+typedef pair<int, string> PiS;
+typedef pair<double, double> Pdd;
+typedef pair<LL, LL> PLL;
+typedef pair<string, int> PSi;
+typedef pair<string, LL> PSL;
+typedef pair<string, string> PSS;
+
+typedef map<int, int> Mapii;
+typedef map<int, string> MapiS;
+typedef map<LL, LL> MapLL;
+typedef map<string, char> MapSc;
+typedef map<string, int> MapSi;
+typedef map<string, string> MapSS;
+typedef map<string, LL> MapSL;
+
+typedef unordered_map<string, LL> HashSL;
+typedef unordered_map<string, double> HashSd;
+typedef unordered_map<string, string> HashSS;
+
+typedef vector<string> Words;
+typedef vector<vector<int>> Matrix2D_i;
+typedef vector<vector<LL>> Matrix2D_L;
+typedef vector<vector<double>> Matrix2D_d;
+
+#define MAX 1000000
+
+ULL Fibonacci(const int N);
+
+int main() {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    ULL fibo;
+    int N;
+
+    cin >> N;
+
+    fibo = Fibonacci(N);
+
+    cout << fibo << "\n";
+
+    return 0;
+}
+
+ULL Fibonacci(const int N) {
+    vector<ULL> dp(MAX + 1);
+    const ULL MOD = 1000000007ULL;
+
+    dp[0] = 0;
+    dp[1] = 1;
+    dp[2] = 1;
+
+    for (int i = 3; i <= N; i++) {
+        dp[i] = (dp[i - 1] + dp[i - 2]) % MOD;
+    }
+
+    return dp[N];
+};
+
