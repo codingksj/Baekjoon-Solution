31f5ef08ebd3e4bb4f9e6394f1fd43c0f04c1502
codingksj <45532564+codingksj@users.noreply.github.com>
2023-10-10 20:58:10 +0900

    Add 10830.cpp

---
+++ 백준/10000~10999/10800~10899/10830.cpp
@@ -0,0 +1,153 @@
+#include<iostream>
+#include<sstream>
+#include<bitset>
+#include<set>
+#include<unordered_map>
+#include<map>
+#include<vector>
+#include<queue>
+#include<deque>
+#include<stack>
+#include<tuple>
+#include<algorithm>
+#include<string>
+#include<numeric>
+#include<cmath>
+#include<cctype>
+#include<cstring>
+#include<iomanip>
+#include<chrono>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef long double LD;
+
+typedef pair<char, char> Pcc;
+typedef pair<char, int> Pci;
+typedef pair<char, string> PcS;
+typedef pair<int, bool> Pib;
+typedef pair<int, int> Pii;
+typedef pair<int, string> PiS;
+typedef pair<double, double> Pdd;
+typedef pair<LL, LL> PLL;
+typedef pair<string, int> PSi;
+typedef pair<string, LL> PSL;
+typedef pair<string, string> PSS;
+
+typedef map<int, int> Mapii;
+typedef map<int, string> MapiS;
+typedef map<LL, LL> MapLL;
+typedef map<string, char> MapSc;
+typedef map<string, int> MapSi;
+typedef map<string, string> MapSS;
+typedef map<string, LL> MapSL;
+
+typedef unordered_map<LL, LL> HashLL;
+typedef unordered_map<string, LL> HashSL;
+typedef unordered_map<string, double> HashSd;
+typedef unordered_map<string, string> HashSS;
+
+typedef vector<string> Words;
+typedef vector<vector<bool>> Matrix2D_b;
+typedef vector<vector<int>> Matrix2D_i;
+typedef vector<vector<LL>> Matrix2D_L;
+typedef vector<vector<double>> Matrix2D_d;
+
+#define MOD 1000LL
+
+Matrix2D_L LoadMatrix(const LL R, const LL C);
+Matrix2D_L CalcMatrixPower(const Matrix2D_L& matrix, const LL power);
+Matrix2D_L CalcMatrixMul(const Matrix2D_L& a, const Matrix2D_L& b);
+string ConvertDecimalToBinary(const LL decimal);
+void PrintMatrix(const Matrix2D_L& matrix, const LL R, const LL C);
+
+int main() {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    Matrix2D_L matrix, poweredMatrix;
+    LL N, power;
+
+    cin >> N >> power;
+
+    matrix = LoadMatrix(N, N);
+    poweredMatrix = CalcMatrixPower(matrix, power);
+
+    PrintMatrix(poweredMatrix, N, N);
+
+    return 0;
+}
+
+Matrix2D_L LoadMatrix(const LL R, const LL C) {
+    Matrix2D_L matrix(R, vector<LL>(C, 0));
+
+    for (vector<LL>& row : matrix) {
+        for (LL& col : row) {
+            cin >> col;
+        }
+    }
+
+    return matrix;
+};
+
+Matrix2D_L CalcMatrixPower(const Matrix2D_L& matrix, const LL power) {
+    Matrix2D_L result;
+    string binary = ConvertDecimalToBinary(power);
+    int M = matrix.size();
+
+    result.resize(M, vector<LL>(M, 0));
+
+    for (int i = 0; i < M; i++) {
+        result[i][i] = 1LL;
+    }
+
+    for (const char& digit : binary) {
+        result = CalcMatrixMul(result, result);
+        if (digit == '1') {
+            result = CalcMatrixMul(result, matrix);
+        }
+    }
+
+    return result;
+};
+
+Matrix2D_L CalcMatrixMul(const Matrix2D_L& a, const Matrix2D_L& b) {
+    Matrix2D_L result;
+    int M = a.size();
+
+    result.resize(M, vector<LL>(M, 0));
+
+    for (int i = 0; i < M; i++) {
+        for (int j = 0; j < M; j++) {
+            for (int k = 0; k < M; k++) {
+                result[i][j] += ((a[i][k] * b[k][j]) % MOD);
+            }
+            result[i][j] %= MOD;
+        }
+    }
+
+    return result;
+};
+
+string ConvertDecimalToBinary(const LL decimal) {
+    string binary = "";
+
+    for (LL i = 1LL; i <= decimal; i <<= 1LL) {
+        binary += (i & decimal) ? '1' : '0';
+    }
+
+    reverse(binary.begin(), binary.end());
+    
+    return binary;
+};
+
+void PrintMatrix(const Matrix2D_L& matrix, const LL R, const LL C) {
+    for (const vector<LL>& row : matrix) {
+        for (const LL& col : row) {
+            cout << col << " ";
+        }
+        cout << "\n";
+    }
+};
