da2ca733875ed7f42a94c00da5c65582e736ad71
codingksj <45532564+codingksj@users.noreply.github.com>
2023-10-12 22:21:55 +0900

    Add 16499.cpp

---
+++ 백준/16000~16999/16400~16499/16499.cpp
@@ -0,0 +1,91 @@
+#include<iostream>
+#include<sstream>
+#include<bitset>
+#include<set>
+#include<unordered_map>
+#include<map>
+#include<vector>
+#include<queue>
+#include<deque>
+#include<stack>
+#include<tuple>
+#include<algorithm>
+#include<string>
+#include<numeric>
+#include<cmath>
+#include<cctype>
+#include<cstring>
+#include<iomanip>
+#include<chrono>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef long double LD;
+
+typedef pair<char, char> Pcc;
+typedef pair<char, int> Pci;
+typedef pair<char, string> PcS;
+typedef pair<int, bool> Pib;
+typedef pair<int, int> Pii;
+typedef pair<int, string> PiS;
+typedef pair<double, double> Pdd;
+typedef pair<LL, LL> PLL;
+typedef pair<string, int> PSi;
+typedef pair<string, LL> PSL;
+typedef pair<string, string> PSS;
+
+typedef tuple<int, int, int> Tiii;
+
+typedef map<int, int> Mapii;
+typedef map<int, string> MapiS;
+typedef map<LL, LL> MapLL;
+typedef map<string, char> MapSc;
+typedef map<string, int> MapSi;
+typedef map<string, string> MapSS;
+typedef map<string, LL> MapSL;
+
+typedef unordered_map<LL, LL> HashLL;
+typedef unordered_map<string, bool> HashSb;
+typedef unordered_map<string, LL> HashSL;
+typedef unordered_map<string, double> HashSd;
+typedef unordered_map<string, string> HashSS;
+
+typedef vector<string> Words;
+typedef vector<vector<bool>> Matrix2D_b;
+typedef vector<vector<int>> Matrix2D_i;
+typedef vector<vector<LL>> Matrix2D_L;
+typedef vector<vector<double>> Matrix2D_d;
+
+set<string> MakeDict(const int N);
+
+int main() {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    set<string> dict;
+    int N, minGroup;
+
+    cin >> N;
+
+    dict = MakeDict(N);
+    minGroup = dict.size();
+
+    cout << minGroup << "\n";
+
+    return 0;
+};
+
+set<string> MakeDict(const int N) {
+    set<string> dict;
+    string word;
+
+    for (int i = 0; i < N; i++) {
+        cin >> word;
+        sort(word.begin(), word.end());
+        dict.insert(word);
+    }
+
+    return dict;
+};
