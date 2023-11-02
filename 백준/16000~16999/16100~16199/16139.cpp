23335fd5079ce8fc8518a68597ca01a4ac820f48
codingksj <45532564+codingksj@users.noreply.github.com>
2023-11-02 20:55:37 +0900

    Update 16139.cpp

--- 백준/16000~16999/16100~16199/16139.cpp
+++ 백준/16000~16999/16100~16199/16139.cpp
@@ -8,64 +8,139 @@
 #include<queue>
 #include<deque>
 #include<stack>
+#include<tuple>
 #include<algorithm>
 #include<string>
 #include<numeric>
+#include<functional>
 #include<cmath>
 #include<cctype>
 #include<cstring>
 #include<iomanip>
 #include<chrono>
 
+//사용자 정의 상수
+#define PI 3.141592653589793
+#define THOUSAND 1E+3
+#define MILLION 1E+6
+#define BILLION 1E+9
+
 using namespace std;
 
+//템플릿 함수
+template<typename T> vector<T> InitVector(const int SIZE, const T INIT) { return vector<T>(SIZE, INIT); }
+template<typename T> vector<vector<T>> Init2DVector(const int R, const int C, const T INIT) { return vector<vector<T>>(R, vector<T>(C, INIT)); }
+
+template<typename T> vector<T> LoadVector(const int SIZE) { vector<T> V(SIZE); for (T& e : V) { cin >> e; } return V; }
+template<typename T> vector<vector<T>> Load2DVector(const int R, const int C) { vector<vector<T>> V(R, vector<T>(C)); for (vector<T>& row : V) { for (T& e : row) { cin >> e; } } return V; }
+template<typename T1, typename T2> vector<pair<T1, T2>> LoadPairVector(const int SIZE) { vector<pair<T1, T2>> V(SIZE); for (pair<T1, T2>& p : V) { cin >> p.first >> p.second; } return V; }
+
+template<typename T> void PrintVector(const vector<T>& V, const string seps) { for (const T& e : V) { cout << e << seps; } }
+template<typename T> void Print2DVector(const vector<vector<T>>& V, const string sepsR, const string sepsC) { for (const vector<T>& row : V) { PrintVector(row, sepsC); cout << sepsR; } }
+
+template<typename T, typename Compare = less<T>> void Sort(vector<T>& V, Compare cmp = Compare()) { sort(V.begin(), V.end(), cmp); }
+template<typename T, typename Compare = less<T>> void Sort2D(vector<vector<T>>& V, Compare cmp = Compare()) { for (vector<T>& row : V) { sort(row.begin(), row.end(), cmp); } }
+template<typename T, typename Compare = less<T>> void StableSort(vector<T>& V, Compare cmp = Compare()) { stable_sort(V.begin(), V.end(), cmp); }
+template<typename T, typename Compare = less<T>> void StableSort2D(vector<vector<T>>& V, Compare cmp = Compare()) { for (vector<T>& row : V) { stable_sort(row.begin(), row.end(), cmp); } }
+
+template<typename T> size_t LowerBoundIndex(const vector<T>& V, const T target) { const size_t index = lower_bound(V.begin(), V.end(), target) - V.begin(); return index; }
+template<typename T> size_t UpperBoundIndex(const vector<T>& V, const T target) { const size_t index = upper_bound(V.begin(), V.end(), target) - V.begin(); return index; }
+
+//사용자 정의 자료형
 typedef long long LL;
 typedef unsigned long long ULL;
 typedef long double LD;
+
+typedef pair<char, char> Pcc;
+typedef pair<char, int> Pci;
+typedef pair<char, string> PcS;
+typedef pair<int, bool> Pib;
 typedef pair<int, int> Pii;
+typedef pair<int, string> PiS;
+typedef pair<double, double> Pdd;
 typedef pair<LL, LL> PLL;
+typedef pair<string, int> PSi;
+typedef pair<string, LL> PSL;
+typedef pair<string, string> PSS;
 
-#define MAX 200000+1
+typedef tuple<int, int, int> Tiii;
 
-int alphaCountPos[MAX][26];
-int alphaCount[26];
+typedef map<int, int> Mapii;
+typedef map<int, string> MapiS;
+typedef map<LL, LL> MapLL;
+typedef map<string, char> MapSc;
+typedef map<string, int> MapSi;
+typedef map<string, string> MapSS;
+typedef map<string, LL> MapSL;
+
+typedef unordered_map<LL, LL> HashLL;
+typedef unordered_map<string, bool> HashSb;
+typedef unordered_map<string, LL> HashSL;
+typedef unordered_map<string, double> HashSd;
+typedef unordered_map<string, string> HashSS;
+
+typedef vector<string> Words;
+typedef vector<vector<bool>> Matrix2D_b;
+typedef vector<vector<int>> Matrix2D_i;
+typedef vector<vector<LL>> Matrix2D_L;
+typedef vector<vector<double>> Matrix2D_d;
+typedef vector<Matrix2D_b> Matrix3D_b;
+typedef vector<Matrix2D_i> Matrix3D_i;
+
+//사용자 지정 함수
+Matrix2D_i MakeLetterCounts(const string& word);
+void DoQuery(const Matrix2D_i& letterCounts, const int Q);
 
 int main() {
-	cin.tie(NULL);
-	ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
 
     string word;
-    char letter;
-    int maxL, pos, count;
-    int L, R;
-    int TC;
+    Matrix2D_i letterCounts;
+    int Q;
 
     cin >> word;
 
-    maxL = word.length();
+    letterCounts = MakeLetterCounts(word);
 
-    for (int length = 0; length < maxL; length++) {
-        pos = word[length] - 'a';
-        alphaCount[pos]++;
-        for (int i = 0; i < 26; i++) {
-            alphaCountPos[length][i] = alphaCount[i];
-        }
-    }
+    cin >> Q;
 
-    cin >> TC;
+    DoQuery(letterCounts, Q);
 
-    while (TC--) {
-        cin >> letter >> L >> R;
-
-        pos = letter - 'a';
-        if (L == R) {
-            cout << (letter == word[R]) << "\n";
-            continue;
-        }
-        count = L ? 
-            alphaCountPos[R][pos] - alphaCountPos[L - 1][pos] :
-            alphaCountPos[R][pos];
-        cout << count << "\n";
-    }
     return 0;
 }
+
+Matrix2D_i MakeLetterCounts(const string& word) {
+    Matrix2D_i letterCounts;
+    vector<int> curCount(26, 0);
+    int maxL = word.length();
+    int index;
+
+    letterCounts.reserve(maxL);
+
+    for (const char& letter : word) {
+        index = letter - 'a';
+        curCount[index]++;
+        letterCounts.push_back(curCount);
+    }
+
+    return letterCounts;
+};
+
+void DoQuery(const Matrix2D_i& letterCounts, const int Q) {
+    int left, right;
+    int val1, val2;
+    int count, index;
+    char alpha;
+
+    for (int i = 0; i < Q; i++) {
+        cin >> alpha >> left >> right;
+
+        index = alpha - 'a';
+        val1 = (left > 0) ? letterCounts[left - 1][index] : 0;
+        val2 = letterCounts[right][index];
+        count = val2 - val1;
+
+        cout << count << "\n";
+    }
+};
