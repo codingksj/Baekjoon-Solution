e60640c0ec894de31fcf207bb7464507fedca0a2
codingksj <45532564+codingksj@users.noreply.github.com>
2023-11-03 23:56:26 +0900

    Add 01707.cpp

---
+++ 백준/01000~01999/01700~01799/01707.cpp
@@ -0,0 +1,184 @@
+#include<iostream>
+#include<sstream>
+#include<bitset>
+#include<set>
+#include<unordered_map>
+#include<map>
+#include<vector>
+#include<queue>
+#include<deque>
+#include<stack>
+#include<tuple>
+#include<algorithm>
+#include<string>
+#include<numeric>
+#include<functional>
+#include<cmath>
+#include<cctype>
+#include<cstring>
+#include<iomanip>
+#include<chrono>
+
+//사용자 정의 상수
+#define PI 3.141592653589793
+#define THOUSAND 1E+3
+#define MILLION 1E+6
+#define BILLION 1E+9
+
+using namespace std;
+
+//템플릿 함수
+template<typename T> vector<T> InitVector(const int SIZE, const T INIT) { return vector<T>(SIZE, INIT); }
+template<typename T> vector<vector<T>> Init2DVector(const int R, const int C, const T INIT) { return vector<vector<T>>(R, vector<T>(C, INIT)); }
+
+template<typename T> vector<T> LoadVector(const int SIZE) { vector<T> V(SIZE); for (T& e : V) { cin >> e; } return V; }
+template<typename T> vector<vector<T>> Load2DVector(const int R, const int C) { vector<vector<T>> V(R, vector<T>(C)); for (vector<T>& row : V) { for (T& e : row) { cin >> e; } } return V; }
+template<typename T1, typename T2> vector<pair<T1, T2>> LoadPairVector(const int SIZE) { vector<pair<T1, T2>> V(SIZE); for (pair<T1, T2>& p : V) { cin >> p.first >> p.second; } return V; }
+
+template<typename T> void PrintVector(const vector<T>& V, const string seps) { for (const T& e : V) { cout << e << seps; } }
+template<typename T> void Print2DVector(const vector<vector<T>>& V, const string sepsR, const string sepsC) { for (const vector<T>& row : V) { PrintVector(row, sepsC); cout << sepsR; } }
+
+template<typename T, typename Compare = less<T>> void Sort(vector<T>& V, Compare cmp = Compare()) { sort(V.begin(), V.end(), cmp); }
+template<typename T, typename Compare = less<T>> void Sort2D(vector<vector<T>>& V, Compare cmp = Compare()) { for (vector<T>& row : V) { sort(row.begin(), row.end(), cmp); } }
+template<typename T, typename Compare = less<T>> void StableSort(vector<T>& V, Compare cmp = Compare()) { stable_sort(V.begin(), V.end(), cmp); }
+template<typename T, typename Compare = less<T>> void StableSort2D(vector<vector<T>>& V, Compare cmp = Compare()) { for (vector<T>& row : V) { stable_sort(row.begin(), row.end(), cmp); } }
+
+template<typename T> size_t LowerBoundIndex(const vector<T>& V, const T target) { const size_t index = lower_bound(V.begin(), V.end(), target) - V.begin(); return index; }
+template<typename T> size_t UpperBoundIndex(const vector<T>& V, const T target) { const size_t index = upper_bound(V.begin(), V.end(), target) - V.begin(); return index; }
+
+//사용자 정의 자료형
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef long double LD;
+
+typedef pair<char, char> Pcc;
+typedef pair<char, int> Pci;
+typedef pair<char, string> PcS;
+typedef pair<int, bool> Pib;
+typedef pair<int, int> Pii;
+typedef pair<int, string> PiS;
+typedef pair<double, double> Pdd;
+typedef pair<LL, LL> PLL;
+typedef pair<string, int> PSi;
+typedef pair<string, LL> PSL;
+typedef pair<string, string> PSS;
+
+typedef tuple<int, int, int> Tiii;
+
+typedef map<int, int> Mapii;
+typedef map<int, string> MapiS;
+typedef map<LL, LL> MapLL;
+typedef map<string, char> MapSc;
+typedef map<string, int> MapSi;
+typedef map<string, string> MapSS;
+typedef map<string, LL> MapSL;
+
+typedef unordered_map<LL, LL> HashLL;
+typedef unordered_map<string, bool> HashSb;
+typedef unordered_map<string, LL> HashSL;
+typedef unordered_map<string, double> HashSd;
+typedef unordered_map<string, string> HashSS;
+
+typedef vector<string> Words;
+typedef vector<vector<bool>> Matrix2D_b;
+typedef vector<vector<int>> Matrix2D_i;
+typedef vector<vector<LL>> Matrix2D_L;
+typedef vector<vector<double>> Matrix2D_d;
+typedef vector<Matrix2D_b> Matrix3D_b;
+typedef vector<Matrix2D_i> Matrix3D_i;
+
+enum Color {
+    RED,
+    BLUE
+};
+
+//사용자 지정 함수
+Matrix2D_i MakeGraph(const int V, const int E);
+void BFS(const Matrix2D_i& graph, vector<int>& bfsCheck, const int V);
+bool CheckBipartite(const Matrix2D_i& graph, vector<int>& bfsCheck, const int V);
+
+int main() {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    Matrix2D_i graph;
+    vector<int> bfsCheck;
+    int TC, V, E;
+    bool isBipartite;
+
+    cin >> TC;
+
+    while (TC--) {
+        cin >> V >> E;
+
+        graph = MakeGraph(V, E);
+        bfsCheck = InitVector(V + 1, 0);
+        BFS(graph, bfsCheck, V);
+        isBipartite = CheckBipartite(graph, bfsCheck, V);
+
+        cout << (isBipartite ? "YES" : "NO") << "\n";
+    }
+
+    return 0;
+}
+
+Matrix2D_i MakeGraph(const int V, const int E) {
+    Matrix2D_i graph = Init2DVector(V + 1, 0, 0);
+    int v, u;
+
+    for (int e = 0; e < E; e++) {
+        cin >> v >> u;
+
+        graph[v].push_back(u);
+        graph[u].push_back(v);
+    }
+
+    return graph;
+};
+
+void BFS(const Matrix2D_i& graph, vector<int>& bfsCheck, const int V) {
+    queue<int> q;
+    int color;
+    int now;
+    
+    for (int i = 1; i <= V; i++) {
+        color = RED;
+
+        if (bfsCheck[i]) {
+            continue;
+        }
+        bfsCheck[i] = color;
+        q.push(i);
+
+        while (!q.empty()) {
+            now = q.front();
+            q.pop();
+
+            (bfsCheck[now] == RED) && (color = BLUE);
+            (bfsCheck[now] == BLUE) && (color = RED);
+
+            for (const int& next : graph[now]) {
+                if (!bfsCheck[next]) {
+                    bfsCheck[next] = color;
+                    q.push(next);
+                }
+            }
+        }
+    }
+};
+
+bool CheckBipartite(const Matrix2D_i& graph, vector<int>& bfsCheck, const int V) {
+    int color1, color2;
+
+    for (int i = 1; i <= V; i++) {
+        color1 = bfsCheck[i];
+        for (const int& next : graph[i]) {
+            color2 = bfsCheck[next];
+            if (color1 == color2) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+};
