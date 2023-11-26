1d1f3d26e5d2c33343739c806f05fabe2548ec45
codingksj <45532564+codingksj@users.noreply.github.com>
2023-11-26 23:13:58 +0900

    Add 01990.cpp

---
+++ 백준/01000~01999/01900~01999/01990.cpp
@@ -0,0 +1,230 @@
+#include<iostream>
+#include<sstream>
+#include<bitset>
+#include<set>
+#include<unordered_map>
+#include<map>
+#include<vector>
+#include<queue>
+#include<deque>
+#include<stack>
+#include<tuple>
+#include<algorithm>
+#include<string>
+#include<numeric>
+#include<functional>
+#include<cmath>
+#include<cctype>
+#include<cstring>
+#include<iomanip>
+#include<chrono>
+
+// ---------- 사용자 정의 상수 ----------
+#define PI 3.141592653589793
+#define THOUSAND 1E+3
+#define MILLION 1E+6
+#define BILLION 1E+9
+#define INF 1E+8
+
+// ---------- 사용자 정의 자료형 ----------
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef long double LD;
+
+typedef pair<char, char> Pcc;
+typedef pair<char, int> Pci;
+typedef pair<char, string> PcS;
+typedef pair<int, bool> Pib;
+typedef pair<int, int> Pii;
+typedef pair<int, string> PiS;
+typedef pair<double, double> Pdd;
+typedef pair<LL, LL> PLL;
+typedef pair<string, int> PSi;
+typedef pair<string, LL> PSL;
+typedef pair<string, string> PSS;
+
+typedef vector<string> Words;
+
+// ---------- 템플릿 자료형 ----------
+template<typename T> struct Matrix2D {
+    vector<vector<T>> m2;
+
+    Matrix2D() {};
+    Matrix2D(const size_t R, const size_t C, const T INIT) {
+        m2 = vector<vector<T>>(R, vector<T>(C, INIT));
+    }
+};
+template<typename T> struct Matrix3D {
+    vector<vector<vector<T>>> m3;
+
+    Matrix3D() {};
+    Matrix3D(const size_t H, const size_t R, const size_t C, const T INIT) {
+        m3 = vector<vector<vector<T>>>(H, vector<vector<T>>(R, vector<T>(C, INIT)));
+    }
+};
+
+// ---------- 템플릿 함수 ----------
+template<typename T> vector<T> InitVector(const size_t SIZE, const T INIT) {
+    return vector<T>(SIZE, INIT);
+}
+template<typename T> vector<vector<T>> Init2DVector(const size_t R, const size_t C, const T INIT) {
+    return vector<vector<T>>(R, InitVector(C, INIT));
+}
+template<typename T> vector<vector<vector<T>>> Init3DVector(const size_t H, const size_t R, const size_t C, const T INIT) {
+    return vector<vector<vector<T>>>(H, Init2DVector(R, C, INIT));
+}
+
+template<typename T1, typename T2> pair<T1, T2> LoadPair() {
+    pair<T1, T2> p;
+    cin >> p.first >> p.second;
+    return p;
+};
+template<typename T> vector<T> LoadVector(const size_t SIZE) {
+    vector<T> V(SIZE);
+    for (T& e : V) {
+        cin >> e;
+    }
+    return V;
+}
+template<typename T1, typename T2> vector<pair<T1, T2>> LoadVector(const size_t SIZE) {
+    vector<pair<T1, T2>> V(SIZE);
+    for (pair<T1, T2>& p : V) {
+        p = LoadPair<T1, T2>();
+    }
+    return V;
+}
+template<typename T> vector<vector<T>> Load2DVector(const size_t R, const size_t C) {
+    vector<vector<T>> V(R, vector<T>(C));
+    for (vector<T>& row : V) {
+        row = LoadVector<T>(C);
+    }
+    return V;
+}
+template<typename T1, typename T2> vector<vector<pair<T1, T2>>> Load2DVector(const size_t R, const size_t C) {
+    vector<vector<pair<T1, T2>>> V(R, vector<pair<T1, T2>>(C));
+    for (vector<pair<T1, T2>>& row : V) {
+        row = LoadVector<T1, T2>(C);
+    }
+    return V;
+}
+
+template<typename T1, typename T2> void PrintPair(const pair<T1, T2>& p, const string sepsPair) {
+    cout << p.first << sepsPair << p.second;
+}
+template<typename T> void PrintVector(const vector<T>& V, const string sepsC) {
+    for (const T& e : V) {
+        cout << e << sepsC;
+    }
+}
+template<typename T1, typename T2> void PrintVector(const vector<pair<T1, T2>>& V, const string sepsR, const string sepsPair) {
+    for (const pair<T1, T2>& p : V) {
+        PrintPair<T1, T2>(p, sepsPair);
+        cout << sepsR;
+    }
+}
+template<typename T> void Print2DVector(const vector<vector<T>>& V, const string sepsR, const string sepsC) {
+    for (const vector<T>& row : V) {
+        PrintVector(row, sepsC);
+        cout << sepsR;
+    }
+}
+template<typename T1, typename T2> void Print2DVector(const vector<vector<pair<T1, T2>>>& V, const string sepsR, const string sepsC, const string sepsPair) {
+    for (const vector<pair<T1, T2>>& row : V) {
+        PrintVector<T1, T2>(row, sepsC, sepsPair);
+        cout << sepsR;
+    }
+}
+
+template<typename T, typename Compare = less<T>> void SortAll(vector<T>& V, Compare cmp = Compare()) {
+    sort(V.begin(), V.end(), cmp);
+}
+template<typename T, typename Compare = less<T>> void SortAll2D(vector<vector<T>>& V, Compare cmp = Compare()) {
+    for (vector<T>& row : V) {
+        Sort(row, cmp);
+    }
+}
+template<typename T, typename Compare = less<T>> void StableSortAll(vector<T>& V, Compare cmp = Compare()) {
+    stable_sort(V.begin(), V.end(), cmp);
+}
+template<typename T, typename Compare = less<T>> void StableSortAll2D(vector<vector<T>>& V, Compare cmp = Compare()) {
+    for (vector<T>& row : V) {
+        StableSort(row, cmp);
+    }
+}
+
+template<typename T> size_t LowerBoundIndex(const vector<T>& V, const T target) {
+    const size_t index = lower_bound(V.begin(), V.end(), target) - V.begin();
+    return index;
+}
+template<typename T> size_t UpperBoundIndex(const vector<T>& V, const T target) {
+    const size_t index = upper_bound(V.begin(), V.end(), target) - V.begin(); 
+    return index;
+}
+
+// ---------- 사용자 정의 함수 및 연산자 ----------
+vector<int> LoadPrimes(const int start, const int end);
+void PrintPrimePelindrome(const vector<int>& primes);
+bool CheckPelindrome(const int N);
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+
+    vector<int> primes;
+    int start, end;
+
+    cin >> start >> end;
+
+    primes = LoadPrimes(start, end);
+    PrintPrimePelindrome(primes);
+
+    return 0;
+}
+
+vector<int> LoadPrimes(const int start, const int end) {
+    vector<int> primes;
+    vector<bool> isPrime;
+    int N = min((int)1E+7, end);
+
+    isPrime = InitVector<bool>(N + 1, true);
+
+    isPrime[0] = isPrime[1] = false;
+
+    for (int i = 2; i * i <= N; i++) {
+        if (isPrime[i]) {
+            for (int j = i * i; j <= N; j += i) {
+                isPrime[j] = false;
+            }
+        }
+    }
+
+    for (int i = 2 * (start / 2) + 1; i <= N; i += 2) {
+        if (isPrime[i]) {
+            primes.push_back(i);
+        }
+    }
+
+    return primes;
+};
+
+void PrintPrimePelindrome(const vector<int>& primes) {
+    for (const int& p : primes) {
+        CheckPelindrome(p) && (cout << p << "\n");
+    }
+    cout << -1 << "\n";
+};
+
+bool CheckPelindrome(const int N) {
+    string s = to_string(N);
+    int length = s.length();
+    
+    for (int i = 0; i < length / 2; i++) {
+        if (s[i] != s[length - 1 - i]) {
+            return false;
+        }
+    }
+
+    return true;
+};
