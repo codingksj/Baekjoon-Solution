9affb07b298f59df6685f6cc6a2714fc4c552a74
codingksj <45532564+codingksj@users.noreply.github.com>
2024-01-09 15:06:38 +0900

    Add 11444.py

---
+++ 백준/11000~11999/11400~11499/11444.py
@@ -0,0 +1,23 @@
+import sys
+
+input = sys.stdin.readline
+
+MOD = (int)((1E+9)+7)
+
+def F(n : int) -> int:
+    if dp.get(n) :
+        return dp[n]
+    
+    k = n//2
+    dp[n] = (F(k)**2 + F(k+1)**2) % MOD if n&1 else (F(k+1)**2 - F(k-1)**2) % MOD
+    
+    return dp[n]
+    
+n = int(input())
+dp = dict()
+
+dp[0] = 0
+dp[1] = 1
+dp[2] = 1
+
+print(F(n))
