a0902ab855ac2b36c4d0228294d97485196cdc6e
codingksj <45532564+codingksj@users.noreply.github.com>
2026-02-11 22:25:52 +0900

    Create 33703.cpp

---
+++ 백준/33000~33999/33700~33799/33703.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+#include<unordered_set>
+#include<ranges>
+#include<numbers>
+#include<print>
+
+using namespace std;
+
+using LL = long long;
+using ULL = unsigned long long;
+using Pii = pair<int, int>;
+using PLL = pair<LL, LL>;
+template<typename T> using V1 = vector<T>;
+template<typename T> using V2 = V1<V1<T>>;
+template<typename T> using V3 = V1<V2<T>>;
+
+namespace CONSTS {
+	constexpr string_view SEPS = " \n";
+	constexpr Pii DT4[4] = { {-1,0}, {0,1} ,{1,0} ,{0,-1} };
+	constexpr LL MOD = 1E+9 + 7, INF = 0x3f3f3f3f, MAX_H = 1 << 5, MAX_N = 10;
+	constexpr int DIGITS = 10, ALPHABETS = 26;
+	constexpr auto Rng = views::iota;
+	constexpr auto Step = views::stride;
+}
+using namespace CONSTS;
+
+void FastIO();
+void SetUp();
+bool Input();
+void Solve();
+void Output();
+
+// ---------- 전역 변수 ----------
+LL res, N;
+
+int main() {
+	FastIO();
+	SetUp();
+
+	Input();
+	Solve();
+	Output();
+
+	return 0;
+}
+
+void FastIO() {
+	ios::sync_with_stdio(false); cin.tie(nullptr);
+	return;
+};
+
+void SetUp() {
+	return;
+};
+
+bool Input() {
+	cin >> N;
+
+	return true;
+};
+
+void Solve() {
+	res = N * (N + 1) / 2;
+	return;
+};
+
+void Output() {
+	println("{}", res);
+	return;
+};
