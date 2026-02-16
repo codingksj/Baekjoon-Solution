f318a91818340fe6c766f49d69d3c1c75f670f74
codingksj <45532564+codingksj@users.noreply.github.com>
2026-02-16 23:09:40 +0900

    Update 35263.cpp

--- 백준/35000~35999/35200~35299/35263.cpp
+++ 백준/35000~35999/35200~35299/35263.cpp
@@ -18,7 +18,7 @@ template<typename T> using V3 = V1<V2<T>>;
 namespace CONSTS {
     constexpr string_view SEPS = " \n";
     constexpr Pii DT4[4] = { {-1,0}, {0,1} ,{1,0} ,{0,-1} };
-    constexpr LL MOD = 1E+9 + 7, INF = 0x3f3f3f3f, MAX_N = 2E+5, MAX_A = 5E+5;
+    constexpr LL MOD = 1E+9 + 7, INF = 0x3f3f3f3f, MAX = 5E+5;
     constexpr int DIGITS = 10, ALPHABETS = 26;
     constexpr auto Rng = views::iota;
     constexpr auto Step = views::stride;
@@ -29,15 +29,15 @@ void FastIO();
 void SetUp();
 bool Input();
 void Solve();
+int Find(int v);
 void Output();
 
-Pii Q[MAX_N + 1];
-LL sums[1 << 3];
-LL factorSum[MAX_A + 1];
-int spf[MAX_A + 1];
-int factorCnt[MAX_A + 1];
-int A[MAX_N + 1];
-int N, M, st, ed;
+LL fSum[MAX + 1];
+bitset<MAX + 1> isPrimes, boom;
+int fCnt[MAX + 1];
+int pa[MAX + 2];
+int A[MAX + 1];
+int N, M, l, r;
 double expect;
 
 int main() {
@@ -57,18 +57,18 @@ void FastIO() {
 };
 
 void SetUp() {
-    iota(spf + 1, spf + MAX_A + 1, 1);
-    for (int i = 2; i <= MAX_A; i++) {
-        if (spf[i] != i) {
+    iota(pa + 1, pa + MAX + 1, 1);
+    isPrimes.set();
+    for (int i = 2; i <= MAX; i++) {
+        if (!isPrimes[i]) {
             continue;
         }
-        for (int j = i; j <= MAX_A; j += i) {
-            if (spf[j] == j) {
-                spf[j] = i;
-            }
-            factorSum[j] += i;
-            factorCnt[j]++;
+        for (int j = i; j <= MAX; j += i) {
+            isPrimes[j] = false;
+            fSum[j] += i;
+            fCnt[j]++;
         }
+        isPrimes[i] = true;
     }
     return;
 };
@@ -79,54 +79,32 @@ bool Input() {
     for (int i : Rng(0, N)) {
         cin >> A[i];
     }
-    for (int i : Rng(0, M)) {
-        cin >> st >> ed;
-        Q[i] = { --st, --ed };
-    }
     return true;
 };
 
 void Solve() {
-    if (M == 0) {
-        expect = accumulate(A, A + N, 0LL);
-        return;
-    }
-    sort(Q, Q + M);
-    tie(st, ed) = Q[0];
-    for (int i = 0; i < st; i++) {
-        sums[1] += A[i];
-    }
-    for (int i = st; i <= ed; i++) {
-        int n = A[i];
-        sums[factorCnt[n]] += factorSum[n];
-    }
-    for (int i = 1; i < M; i++) {
-        const auto& [nSt, nEd] = Q[i];
-        if (nSt > ed) {
-            for (int j = ed + 1; j < nSt; j++) {
-                sums[1] += A[j];
-            }
-            for (int j = nSt; j <= nEd; j++) {
-                int n = A[j];
-                sums[factorCnt[n]] += factorSum[n];
-            }
-            st = nSt, ed = nEd;
-        }
-        else if (nEd > ed) {
-            for (int j = ed + 1; j <= nEd; j++) {
-                int n = A[j];
-                sums[factorCnt[n]] += factorSum[n];
-            }
-            ed = nEd;
+    LL sums[8] = { 0, };
+    for (int i : Rng(0, M)) {
+        cin >> l >> r;
+        l--, r--;
+        for (int v = Find(l); v <= r; v = Find(v)) {
+            int n = A[v];
+            sums[fCnt[n]] += fSum[n];
+            boom[v] = true;
+            pa[v] = Find(v + 1);
         }
     }
-    for (int j = ed + 1; j < N; j++) {
-        sums[1] += A[j];
+    for (int i : Rng(0, N)) {
+        sums[1] += A[i] * !boom[i];
     }
     for (int n = 1; n <= 7; n++) {
         expect += (double)sums[n] / n;
     }
     return;
+};
+
+int Find(int v) {
+    return v == pa[v] ? v : pa[v] = Find(pa[v]);
 };
 
 void Output() {
