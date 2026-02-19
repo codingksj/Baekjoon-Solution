a448ab6eaf9fddc224a12b0c69b7f95e459f07f0
codingksj <45532564+codingksj@users.noreply.github.com>
2026-02-19 13:02:40 +0900

    Create 34687.cpp

---
+++ 백준/34000~34999/34600~34699/34687.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+#include<unordered_set>
+#include<ranges>
+#include<print>
+
+using namespace std;
+
+using LL = long long;
+using ULL = unsigned long long;
+using Pii = pair<int, int>;
+using PLL = pair<LL, LL>;
+template<typename T> using V1 = vector<T>;
+template<typename T> using V2 = V1<V1<T>>;
+template<typename T> using V3 = V1<V2<T>>;
+
+namespace CONSTS {
+    constexpr string_view SEPS = " \n";
+    constexpr Pii DT4[4] = { {-1,0}, {0,1} ,{1,0} ,{0,-1} };
+    constexpr ULL MOD = 1E+9 + 7, INF = 0x3f3f3f3f; 
+    constexpr int DIGITS = 10, ALPHABETS = 26, MAX = 50000;
+    constexpr auto Rng = views::iota;
+    constexpr auto Step = views::stride;
+}
+using namespace CONSTS;
+
+void FastIO();
+void SetUp();
+bool Input();
+void Solve();
+void Output();
+
+string res;
+int N, M;
+
+int main() {
+    FastIO();
+    SetUp();
+
+    Input();
+    Solve();
+    Output();
+
+    return 0;
+}
+
+void FastIO() {
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    return;
+};
+
+void SetUp() {
+    return;
+};
+
+bool Input() {
+    cin >> N >> M;
+
+    return true;
+};
+
+void Solve() {
+    res = 100 * M >= 81 * N ? "yaho" : "no";
+};
+
+void Output() {
+    println("{}", res);
+    return;
+};
