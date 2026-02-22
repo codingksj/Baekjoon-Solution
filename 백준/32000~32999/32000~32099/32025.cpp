17310c2672245ad43c89132edead02767ed8c6cf
codingksj <45532564+codingksj@users.noreply.github.com>
2026-02-22 14:57:14 +0900

    Create 32025.cpp

---
+++ 백준/32000~32999/32000~32099/32025.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+#include<unordered_set>
+#include<ranges>
+#include<print>
+
+using namespace std;
+
+using LL = long long;
+using ULL = unsigned long long;
+using Pii = pair<int, int>;
+template<typename T> using V1 = vector<T>;
+template<typename T> using V2 = V1<V1<T>>;
+template<typename T> using V3 = V1<V2<T>>;
+
+namespace CONSTS {
+    constexpr string_view SEPS = " \n";
+    constexpr Pii DT4[4] = { {-1,0}, {0,1} ,{1,0} ,{0,-1} };
+    constexpr ULL MOD = 1E+9 + 7, INF = 0x3f3f3f3f, MAX = 1E+5;
+    constexpr int DIGITS = 10, ALPHABETS = 26;
+    constexpr auto Rng = views::iota;
+    constexpr auto Step = views::stride;
+}
+using namespace CONSTS;
+
+void FastIO();
+void SetUp();
+bool Input();
+void Solve();
+void Output();
+
+int H, W, r;
+
+int main() {
+    FastIO();
+    SetUp();
+
+    Input();
+    Solve();
+    Output();
+
+    return 0;
+}
+
+void FastIO() {
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    return;
+};
+
+void SetUp() {
+    return;
+};
+
+bool Input() {
+    cin >> H >> W;
+
+    return true;
+};
+
+void Solve() {
+    r = min(H, W) * 50;
+};
+
+void Output() {
+    println("{}", r);
+    return;
+};
