d898beebddfa7e72cf2c61bb353eb127aa0bb56b
codingksj <45532564+codingksj@users.noreply.github.com>
2026-02-27 17:56:39 +0900

    Create 20222.cpp

---
+++ 백준/20000~20999/20200~20299/20222.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+#include<ranges>
+#include<print>
+#include<unordered_set>
+#include<random>
+
+using namespace std;
+
+using LL = long long;
+using ULL = unsigned long long;
+using Pii = pair<int, int>;
+using PLL = pair<LL, LL>;
+using Arr = array<int, 1 << 19>;
+template<typename T> using V1 = vector<T>;
+template<typename T> using V2 = V1<V1<T>>;
+template<typename T> using V3 = V1<V2<T>>;
+
+namespace CONSTS {
+    constexpr string_view SEPS = " \n";
+    constexpr Pii DT4[4] = { {-1,0}, {0,1} ,{1,0} ,{0,-1} };
+    constexpr ULL MOD = 1E+9 + 7, INF = 0x3f3f3f3f;
+    constexpr int DIGITS = 10, ALPHABETS = 26, MAX = 5E+5, LIMIT = 1E+4;
+    constexpr auto Rng = views::iota;
+    constexpr auto Step = views::stride;
+}
+using namespace CONSTS;
+
+void FastIO();
+void SetUp();
+bool Input();
+void Solve();
+void Output();
+
+LL N, K, mnJar;
+
+int main() {
+    FastIO();
+    SetUp();
+
+    Input();
+    Solve();
+    Output();
+
+    return 0;
+}
+
+void FastIO() {
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    return;
+};
+
+void SetUp() {
+    return;
+};
+
+bool Input() {
+    cin >> N >> K;
+
+    return true;
+};
+
+void Solve() {
+    mnJar = N / ((N + K - 1) / K);
+    return;
+};
+
+void Output() {
+    println("{}", mnJar);
+    return;
+};
