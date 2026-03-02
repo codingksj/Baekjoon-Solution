bd52bedc7815e5216196bb302efdb51e07d171f9
codingksj <45532564+codingksj@users.noreply.github.com>
2026-03-02 22:40:03 +0900

    Create 24270.cpp

---
+++ 백준/24000~24999/24200~24299/24270.cpp
@@ -0,0 +1,75 @@
+#include<bits/stdc++.h>
+#include<unordered_set>
+#include<ranges>
+#include<print>
+#include<random>
+
+using namespace std;
+
+using LL = long long;
+using ULL = unsigned long long;
+using Pii = pair<int, int>;
+using PLL = pair<LL, LL>;
+template<typename T> using V1 = vector<T>;
+template<typename T> using V2 = V1<V1<T>>;
+template<typename T> using V3 = V1<V2<T>>;
+
+namespace CONSTS {
+    constexpr string_view SEPS = " \n";
+    constexpr Pii DT4[4] = { {-1,0}, {0,1} ,{1,0} ,{0,-1} };
+    constexpr ULL MOD = 1E+9 + 7, INF = 0x3f3f3f3f, MAX = 100;
+    constexpr int DIGITS = 10, ALPHABETS = 26;
+    constexpr auto Rng = views::iota;
+    constexpr auto Step = views::stride;
+}
+using namespace CONSTS;
+
+void FastIO();
+void SetUp();
+bool Input();
+void Solve();
+void Output();
+
+LL N, K, sum, s, res = 1;
+
+int main() {
+    FastIO();
+    SetUp();
+
+    Input();
+    Solve();
+    Output();
+
+    return 0;
+}
+
+void FastIO() {
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    return;
+};
+
+void SetUp() {
+    return;
+};
+
+bool Input() {
+    cin >> N >> K;
+
+    return true;
+};
+
+void Solve() {
+    sum = K;
+    for (int i : Rng(0, N)) {
+        cin >> s;
+        sum -= s;
+    }
+    for (int n = 1; n <= N; n++) {
+        res = (res * (sum + n)) % MOD;
+    }
+};
+
+void Output() {
+    println("{}", res);
+    return;
+};
