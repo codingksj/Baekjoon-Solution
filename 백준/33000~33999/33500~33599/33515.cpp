1cea48371d42d3d4e5e7550ea48be42dcdeed9ee
codingksj <45532564+codingksj@users.noreply.github.com>
2026-03-06 23:50:59 +0900

    Create 33515.cpp

---
+++ 백준/33000~33999/33500~33599/33515.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+#include<ranges>
+#include<print>
+
+using namespace std;
+
+using LL = long long;
+using ULL = unsigned long long;
+using Pii = pair<int, int>;
+using PLL = pair<LL, LL>;
+using Ti3 = tuple<int, int, int>;
+template<typename T> using V1 = vector<T>;
+template<typename T> using V2 = V1<V1<T>>;
+template<typename T> using V3 = V1<V2<T>>;
+
+namespace CONSTS {
+    constexpr string_view SEPS = " \n", RES = "-0+";
+    constexpr Pii DT4[4] = { {-1,0}, {0,1}, {1,0}, {0,-1} };
+    constexpr ULL MOD = 1E+9 + 7, INF = 0x3f3f3f3f, MAX = 20;
+    constexpr int DIGITS = 10, ALPHABETS = 26;
+    constexpr auto Rng = views::iota;
+    constexpr auto Step = views::stride;
+}
+using namespace CONSTS;
+
+void FastIO();
+void SetUp();
+bool Input();
+void Solve();
+void Output();
+
+int T1, T2;
+
+int main() {
+    FastIO();
+    SetUp();
+
+    Input();
+    Solve();
+    Output();
+
+    return 0;
+}
+
+void FastIO() {
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    return;
+};
+
+void SetUp() {
+    return;
+};
+
+bool Input() {
+    cin >> T1 >> T2;
+
+    return true;
+};
+
+void Solve() {
+    return;
+};
+
+void Output() {
+    println("{}", min(T1, T2));
+    return;
+};
