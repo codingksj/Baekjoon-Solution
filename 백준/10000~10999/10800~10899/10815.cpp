dd2a22ca76bd47f3a8a80f05c1d25d03a8d88452
codingksj <45532564+codingksj@users.noreply.github.com>
2026-03-07 19:57:18 +0900

    Create 10815.cpp

---
+++ 백준/10000~10999/10800~10899/10815.cpp
@@ -0,0 +1,76 @@
+#include<bits/stdc++.h>
+#include<ranges>
+#include<print>
+
+using namespace std;
+
+using LL = long long;
+using ULL = unsigned long long;
+using Pii = pair<int, int>;
+using PLL = pair<LL, LL>;
+using Ti3 = tuple<int, int, int>;
+template<typename T> using V1 = vector<T>;
+template<typename T> using V2 = V1<V1<T>>;
+template<typename T> using V3 = V1<V2<T>>;
+
+namespace CONSTS {
+    constexpr string_view SEPS = " \n";
+    constexpr Pii DT4[4] = { {-1,0}, {0,1}, {1,0}, {0,-1} };
+    constexpr ULL MOD = 1E+9 + 7, INF = 0x3f3f3f3f, MAX = 1E+7;
+    constexpr int DIGITS = 10, ALPHABETS = 26;
+    constexpr auto Rng = views::iota;
+    constexpr auto Step = views::stride;
+}
+using namespace CONSTS;
+
+void FastIO();
+void SetUp();
+bool Input();
+void Solve();
+void Output();
+
+bitset<2 * MAX + 1> visChk;
+int N, M, card;
+
+int main() {
+    FastIO();
+    SetUp();
+
+    Input();
+    Solve();
+    Output();
+
+    return 0;
+}
+
+void FastIO() {
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    return;
+};
+
+void SetUp() {
+    return;
+};
+
+bool Input() {
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        cin >> card;
+        visChk[card + MAX] = true;
+    }
+    cin >> M;
+
+    return true;
+};
+
+void Solve() {
+    for (int i = 0; i < M; i++) {
+        cin >> card;
+        print("{}{}", (int)visChk[card + MAX], SEPS[i == M - 1]);
+    }
+};
+
+void Output() {
+    return;
+};
