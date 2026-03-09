64f2702b50f608441168b551ecb96a003fd6dffc
codingksj <45532564+codingksj@users.noreply.github.com>
2026-03-09 22:17:18 +0900

    Update 05430.cpp

--- 백준/05000~05999/05400~05499/05430.cpp
+++ 백준/05000~05999/05400~05499/05430.cpp
@@ -1,85 +1,129 @@
-#include <iostream>
-#include <string>
-#include <deque>
-#include <algorithm>
+#include<bits/stdc++.h>
+#include<ranges>
+#include<print>
+#include<random>
 
 using namespace std;
 
-int main(){
-    deque<int> DQ;
-    string AC, list, tmp;
+using LL = long long;
+using ULL = unsigned long long;
+using Pii = pair<int, int>;
+using PLL = pair<LL, LL>;
+template<typename T> using V1 = vector<T>;
+template<typename T> using V2 = V1<V1<T>>;
+template<typename T> using V3 = V1<V2<T>>;
 
-    int N, TC;
-    bool reverse, error;
+namespace CONSTS {
+    constexpr string_view SEPS = " \n";
+    constexpr Pii DT4[4] = { {-1,0}, {0,1}, {1,0}, {0,-1} };
+    constexpr ULL MOD = 1E+9 + 7, INF = 0x3f3f3f3f, MAX = 1E+5;
+    constexpr int DIGITS = 10, ALPHABETS = 26;
+    constexpr auto Rng = views::iota;
+    constexpr auto Step = views::stride;
+}
+using namespace CONSTS;
 
-    scanf("%d", &TC);
+void FastIO();
+void SetUp();
+bool Input();
+void Solve();
+void Output();
+
+string X, cmd;
+int TC, N, l, r;
+bool isError, isRev;
+
+int main() {
+    FastIO();
+    SetUp();
+
+    cin >> TC;
 
     while (TC--) {
-        //초기화 및 입력
-        DQ.clear();
-        tmp = "";
-        reverse = error = false;
-        cin >> AC;
-        cin >> N;
-        cin >> list;
-        
-        //숫자 추출
-        for (int i = 0; i < list.length(); i++) {
-            if (isdigit(list[i])) {
-                tmp += list[i];
-            }
-            else {
-                if (!tmp.empty()) {
-                    DQ.push_back(stoi(tmp));
-                    tmp.clear();
-                }
-            }
-        }
-
-        //명령어 실행
-        for (auto func : AC) {
-            if (func == 'R') {
-                reverse = !reverse;
-            }
-            else {
-                if (DQ.empty()) {
-                    error = true;
-                    break;
-                }
-                if (reverse)
-                    DQ.pop_back();
-                else
-                    DQ.pop_front();
-            }
-        }
-
-        //실행 결과
-        if (error) {
-            cout << "error" << "\n";
-        }
-        else if (DQ.size() == 0) {
-            cout << "[]" << "\n";
-        }
-        else{
-            cout << "[";
-            if (reverse) {
-                for (auto o = DQ.rbegin(); o != DQ.rend(); o++) {
-                    if (o == DQ.rend() - 1)
-                        cout << *o;
-                    else
-                        cout << *o << ',';
-                }
-            }
-            else {
-                for (auto o = DQ.begin(); o != DQ.end(); o++) {
-                    if (o == DQ.end() - 1)
-                        cout << *o;
-                    else
-                        cout << *o << ',';
-                }
-            }
-            cout << "]\n";
-        }
-
+        Input();
+        Solve();
+        Output();
     }
+
+    return 0;
 }
+
+void FastIO() {
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    return;
+};
+
+void SetUp() {
+    return;
+};
+
+bool Input() {
+    cin >> cmd >> N >> X;
+
+    return true;
+};
+
+void Solve() {
+    isError = false;
+    isRev = false;
+    int st = 0, ed = N - 1;
+    for (char ch : cmd) {
+        if (ch == 'R') {
+            isRev = !isRev;
+        }
+        else {
+            if (st > ed) {
+                isError = true;
+                return;
+            }
+            isRev ? ed-- : st++;
+        }
+    }
+    int L = X.size();
+    l = 1, r = L - 2;
+    for (int skip = 0; skip < st && l < L; l++) {
+        if (X[l] == ',') {
+            skip++;
+        }
+    }
+    for (int skip = 0; skip < N - 1 - ed && r >= 0; r--) {
+        if (X[r] == ',') {
+            skip++;
+        }
+    }
+};
+
+void Output() {
+    if (isError) {
+        println("error");
+    }
+    else {
+        print("[");
+        int n = 0;
+        if (isRev) {
+            int mul = 1;
+            for (int i = r; i >= l; i--) {
+                char ch = X[i];
+                if (isdigit(ch)) {
+                    n += mul * (ch - '0');
+                    mul *= 10;
+                }
+                else {
+                    print("{},", n);
+                    n = 0;
+                    mul = 1;
+                }
+            }
+            if (n) {
+                print("{}", n);
+            }
+        }
+        else {
+            for (int i = l; i <= r; i++) {
+                print("{}", X[i]);
+            }
+        }
+        println("]");
+    }
+    return;
+};
