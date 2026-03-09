be9a0e4bedda8484a9094fcaf62a4f34b2819bb3
codingksj <45532564+codingksj@users.noreply.github.com>
2026-03-09 21:16:20 +0900

    Update 05525.cpp

--- 백준/05000~05999/05500~05599/05525.cpp
+++ 백준/05000~05999/05500~05599/05525.cpp
@@ -1,36 +1,84 @@
-#include<iostream>
-#include<set>
-#include<map>
-#include<vector>
-#include<algorithm>
-#include<string>
-#include<cmath>
+#include<bits/stdc++.h>
+#include<ranges>
+#include<print>
+#include<random>
 
 using namespace std;
 
-int main() {
-	cin.tie(NULL);
-	ios::sync_with_stdio(false);
-	string S;
-	int N, M;
-	int ans = 0;
-	cin >> N;
-	cin >> M;
-	cin >> S;
-	for (int i = 0; i < M; i++) {
-		int IOI = 0;
-		if (S[i] == 'O') {
-			continue;
-		}
-		while (S[i + 1] == 'O' && S[i + 2] == 'I') {
-			IOI++;
-			if (IOI == N) {
-				ans++;
-				IOI--;
-			}
-			i += 2;
-		}
-	}
-	cout << ans << '\n';
-	return 0;
+using LL = long long;
+using ULL = unsigned long long;
+using Pii = pair<int, int>;
+using PLL = pair<LL, LL>;
+template<typename T> using V1 = vector<T>;
+template<typename T> using V2 = V1<V1<T>>;
+template<typename T> using V3 = V1<V2<T>>;
+
+namespace CONSTS {
+    constexpr string_view SEPS = " \n";
+    constexpr Pii DT4[4] = { {-1,0}, {0,1}, {1,0}, {0,-1} };
+    constexpr ULL MOD = 1E+9 + 7, INF = 0x3f3f3f3f, MAX = 1E+5;
+    constexpr int DIGITS = 10, ALPHABETS = 26;
+    constexpr auto Rng = views::iota;
+    constexpr auto Step = views::stride;
 }
+using namespace CONSTS;
+
+void FastIO();
+void SetUp();
+bool Input();
+void Solve();
+void Output();
+
+string s;
+int N, L, res;
+
+int main() {
+    FastIO();
+    SetUp();
+
+    Input();
+    Solve();
+    Output();
+
+    return 0;
+}
+
+void FastIO() {
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    return;
+};
+
+void SetUp() {
+    return;
+};
+
+bool Input() {
+    cin >> N >> L >> s;
+
+    return true;
+};
+
+void Solve() {
+    for (int i = 0; i < L;) {
+        if (s[i++] != 'I') {
+            continue;
+        }
+        int len = 0;
+        int j = i;
+        for (; j <= L - 2; j += 2) {
+            if (s[j] == 'O' && s[j + 1] == 'I') {
+                len++;
+            }
+            else {
+                break;
+            }
+        }
+        i = j;
+        res += max(0, len - N + 1);
+    }
+};
+
+void Output() {
+    println("{}", res);
+    return;
+};
