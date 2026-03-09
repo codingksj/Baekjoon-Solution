4fca495b36fbd195c99268e2327e258babed2206
codingksj <45532564+codingksj@users.noreply.github.com>
2026-03-09 23:40:42 +0900

    Update 09019.cpp

--- 백준/09000~09999/09000~09099/09019.cpp
+++ 백준/09000~09999/09000~09099/09019.cpp
@@ -1,149 +1,111 @@
-#include<iostream>
-#include<sstream>
-#include<bitset>
-#include<set>
-#include<unordered_map>
-#include<map>
-#include<vector>
-#include<queue>
-#include<deque>
-#include<stack>
-#include<tuple>
-#include<algorithm>
-#include<string>
-#include<numeric>
-#include<functional>
-#include<cmath>
-#include<cctype>
-#include<cstring>
-#include<iomanip>
-#include<chrono>
-
-//사용자 정의 상수
-#define PI 3.141592653589793
-#define THOUSAND 1E+3
-#define MILLION 1E+6
-#define BILLION 1E+9
+#include<bits/stdc++.h>
+#include<ranges>
+#include<print>
+#include<random>
 
 using namespace std;
 
-//템플릿 함수
-template<typename T> vector<T> InitVector(const int SIZE, const T INIT) { return vector<T>(SIZE, INIT); }
-template<typename T> vector<vector<T>> Init2DVector(const int R, const int C, const T INIT) { return vector<vector<T>>(R, vector<T>(C, INIT)); }
+using LL = long long;
+using ULL = unsigned long long;
+using Pii = pair<int, int>;
+using PLL = pair<LL, LL>;
+using Register = array<int, 4>;
+using FuncPtr = void (*)(Register&);
+using Data = tuple<Register, ULL, ULL>;
+template<typename T> using V1 = vector<T>;
+template<typename T> using V2 = V1<V1<T>>;
+template<typename T> using V3 = V1<V2<T>>;
 
-template<typename T> vector<T> LoadVector(const int SIZE) { vector<T> V(SIZE); for (T& e : V) { cin >> e; } return V; }
-template<typename T> vector<vector<T>> Load2DVector(const int R, const int C) { vector<vector<T>> V(R, vector<T>(C)); for (vector<T>& row : V) { for (T& e : row) { cin >> e; } } return V; }
-template<typename T1, typename T2> vector<pair<T1, T2>> LoadPairVector(const int SIZE) { vector<pair<T1, T2>> V(SIZE); for (pair<T1, T2>& p : V) { cin >> p.first >> p.second; } return V; }
+namespace CONSTS {
+    constexpr string_view SEPS = " \n", DSLR = "DSLR";
+    constexpr Pii DT4[4] = { {-1,0}, {0,1}, {1,0}, {0,-1} };
+    constexpr ULL MOD = 1E+9 + 7, INF = 0x3f3f3f3f, MAX = 1E+4, MASK = 3, SHIFT = 2;
+    constexpr int DIGITS = 10, ALPHABETS = 26;
+    constexpr auto Rng = views::iota;
+    constexpr auto Step = views::stride;
+}
+using namespace CONSTS;
 
-template<typename T> void PrintVector(const vector<T>& V, const string seps) { for (const T& e : V) { cout << e << seps; } }
-template<typename T> void Print2DVector(const vector<vector<T>>& V, const string sepsR, const string sepsC) { for (const vector<T>& row : V) { PrintVector(row, sepsC); cout << sepsR; } }
+void FastIO();
+void SetUp();
+bool Input();
+void Solve();
+void Output();
 
-template<typename T, typename Compare = less<T>> void Sort(vector<T>& V, Compare cmp = Compare()) { sort(V.begin(), V.end(), cmp); }
-template<typename T, typename Compare = less<T>> void Sort2D(vector<vector<T>>& V, Compare cmp = Compare()) { for (vector<T>& row : V) { sort(row.begin(), row.end(), cmp); } }
-template<typename T, typename Compare = less<T>> void StableSort(vector<T>& V, Compare cmp = Compare()) { stable_sort(V.begin(), V.end(), cmp); }
-template<typename T, typename Compare = less<T>> void StableSort2D(vector<vector<T>>& V, Compare cmp = Compare()) { for (vector<T>& row : V) { stable_sort(row.begin(), row.end(), cmp); } }
-
-template<typename T> int LowerBoundIndex(const vector<T>& V, const T target) { const int index = lower_bound(V.begin(), V.end(), target) - V.begin(); return index; }
-template<typename T> int UpperBoundIndex(const vector<T>& V, const T target) { const int index = upper_bound(V.begin(), V.end(), target) - V.begin(); return index; }
-
-//사용자 정의 자료형
-typedef long long LL;
-typedef unsigned long long ULL;
-typedef long double LD;
-
-typedef pair<char, char> Pcc;
-typedef pair<char, int> Pci;
-typedef pair<char, string> PcS;
-typedef pair<int, bool> Pib;
-typedef pair<int, int> Pii;
-typedef pair<int, string> PiS;
-typedef pair<double, double> Pdd;
-typedef pair<LL, LL> PLL;
-typedef pair<string, int> PSi;
-typedef pair<string, LL> PSL;
-typedef pair<string, string> PSS;
-
-typedef tuple<int, int, int> Tiii;
-
-typedef map<int, int> Mapii;
-typedef map<int, string> MapiS;
-typedef map<LL, LL> MapLL;
-typedef map<string, char> MapSc;
-typedef map<string, int> MapSi;
-typedef map<string, string> MapSS;
-typedef map<string, LL> MapSL;
-
-typedef unordered_map<LL, LL> HashLL;
-typedef unordered_map<string, bool> HashSb;
-typedef unordered_map<string, LL> HashSL;
-typedef unordered_map<string, double> HashSd;
-typedef unordered_map<string, string> HashSS;
-
-typedef vector<string> Words;
-typedef vector<vector<bool>> Matrix2D_b;
-typedef vector<vector<int>> Matrix2D_i;
-typedef vector<vector<LL>> Matrix2D_L;
-typedef vector<vector<double>> Matrix2D_d;
-typedef vector<Matrix2D_b> Matrix3D_b;
-typedef vector<Matrix2D_i> Matrix3D_i;
-
-//사용자 지정 함수
-string FindMinCommands(vector<bool>& bfsCheck, const int src, const int dest);
+int table[MAX][4];
+char op[MAX];
+char res[MAX];
+int par[MAX];
+int visChk[MAX];
+int TC, A, B, id, cnt;
 
 int main() {
-    cin.tie(NULL);
-    ios::sync_with_stdio(false);
-
-    vector<bool> bfsCheck;
-    string commands;
-    int TC;
-    int A, B;
+    FastIO();
+    SetUp();
 
     cin >> TC;
 
     while (TC--) {
-        cin >> A >> B;
-
-        bfsCheck = InitVector<bool>(10 * THOUSAND, false);
-        commands = FindMinCommands(bfsCheck, A, B);
-
-        cout << commands << "\n";
+        Input();
+        Solve();
+        Output();
     }
 
     return 0;
 }
 
-string FindMinCommands(vector<bool>& bfsCheck, const int src, const int dest) {
-    vector<int> numbers;
-    queue<PSi> q;
-    const string DSLR = "DSLR";
-    string commands;
-    int N = src;
-    int D, S, L, R;
+void FastIO() {
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    return;
+};
 
-    q.push({ "", N });
-    bfsCheck[N] = true;
+void SetUp() {
+    for (int n = 0; n < MAX; n++) {
+        table[n][0] = (2 * n) % MAX;
+        table[n][1] = n == 0 ? MAX - 1 : n - 1;
+        table[n][2] = (n % 1000) * DIGITS + n / 1000;
+        table[n][3] = (n / 10) + 1000 * (n % 10);
+    }
+    return;
+};
+
+bool Input() {
+    cin >> A >> B;
+
+    return true;
+};
+
+void Solve() {
+    queue<int> q;
+    q.push(A);
+    visChk[A] = ++id;
+    cnt = 0;
 
     while (!q.empty()) {
-        commands = q.front().first;
-        N = q.front().second;
-        q.pop();
-        if (N == dest) {
+        int n = q.front(); q.pop();
+        if (n == B) {
             break;
         }
-        D = (2 * N) % 10000;
-        S = (N + 9999) % 10000;
-        L = (N % 1000) * 10 + N / 1000;
-        R = (N % 10) * 1000 + N / 10;
-        numbers = { D,S,L,R };
-        for (int i = 0; i < 4;i++) {
-            N = numbers[i];
-            if (!bfsCheck[N]) {
-                bfsCheck[N] = true;
-                q.push({ commands + DSLR[i], N });
+        for (int i = 0; i < 4; i++) {
+            int nn = table[n][i];
+            if (visChk[nn] != id) {
+                q.push(nn);
+                visChk[nn] = id;
+                par[nn] = n;
+                op[nn] = DSLR[i];
             }
         }
     }
+    for (int n = B; n != A; n = par[n]) {
+        res[cnt++] = op[n];
+    }
+    return;
+};
 
-    return commands;
+void Output() {
+    for (int i = cnt - 1; i >= 0; i--) {
+        print("{}", res[i]);
+    }
+    println("");
+    return;
 };
