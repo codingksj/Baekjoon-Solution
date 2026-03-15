aecfa4af5246baa33ed4d9bcc88b229ef1b1592a
codingksj <45532564+codingksj@users.noreply.github.com>
2026-03-15 23:11:47 +0900

    Create 32775.cpp

---
+++ 백준/32000~32999/32700~32799/32775.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+#include<ranges>
+#include<print>
+
+using namespace std;
+
+using LL = long long;
+using ULL = unsigned long long;
+using Pii = pair<int, int>;
+using PLL = pair<LL, LL>;
+template<typename T> using V1 = vector<T>;
+template<typename T> using V2 = V1<V1<T>>;
+template<typename T> using V3 = V1<V2<T>>;
+
+namespace CONSTS {
+	constexpr string_view SEPS = " \n";
+	constexpr Pii DT4[4] = { {-1,0}, {0,1} ,{1,0} ,{0,-1} };
+	constexpr ULL MOD = 1E+9 + 7, INF = 0x3f3f3f3f;
+	constexpr int DIGITS = 10, ALPHABETS = 26, MAX = 100, VAL = 20;
+	constexpr auto Rng = views::iota;
+	constexpr auto Step = views::stride;
+}
+using namespace CONSTS;
+
+void FastIO();
+void SetUp();
+bool Input();
+void Solve();
+void Output();
+
+string res;
+int s, f;
+
+int main() {
+	FastIO();
+	SetUp();
+
+	Input();
+	Solve();
+	Output();
+
+	return 0;
+}
+
+void FastIO() {
+	ios::sync_with_stdio(false); cin.tie(nullptr);
+	return;
+};
+
+void SetUp() {
+	return;
+};
+
+bool Input() {
+	cin >> s >> f;
+
+	return true;
+};
+
+void Solve() {
+	res = s <= f ? "high speed rail" : "flight";
+};
+
+void Output() {
+	println("{}", res);
+	return;
+};
