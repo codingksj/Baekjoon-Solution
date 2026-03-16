e731f164a0fb486df122155b203af26e4cd89535
codingksj <45532564+codingksj@users.noreply.github.com>
2026-03-16 23:26:37 +0900

    Create 34217.cpp

---
+++ 백준/34000~34999/34200~34299/34217.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+#include<ranges>
+#include<print>
+
+using namespace std;
+
+using LL = long long;
+using ULL = unsigned long long;
+using Pii = pair<int, int>;
+using PLL = pair<LL, LL>;
+template<typename T> using V1 = vector<T>;
+template<typename T> using V2 = V1<V1<T>>;
+template<typename T> using V3 = V1<V2<T>>;
+
+namespace CONSTS {
+	constexpr string_view SEPS = " \n";
+	constexpr Pii DT4[4] = { {-1,0}, {0,1} ,{1,0} ,{0,-1} };
+	constexpr ULL MOD = 1E+9 + 7, INF = 0x3f3f3f3f;
+	constexpr int DIGITS = 10, ALPHABETS = 26, MAX = 18, LIMIT = 100;
+	constexpr auto Rng = views::iota;
+	constexpr auto Step = views::stride;
+}
+using namespace CONSTS;
+
+void FastIO();
+void SetUp();
+bool Input();
+void Solve();
+void Output();
+
+int A, B, C, D, res;
+
+int main() {
+	FastIO();
+	SetUp();
+
+	Input();
+	Solve();
+	Output();
+
+	return 0;
+}
+
+void FastIO() {
+	ios::sync_with_stdio(false); cin.tie(nullptr);
+	return;
+};
+
+void SetUp() {
+	return;
+};
+
+bool Input() {
+	cin >> A >> B >> C >> D;
+
+	return true;
+};
+
+void Solve() {
+	res = (B + D) - (A + C);
+};
+
+void Output() {
+	println("{}", !res ? "Either" : res > 0 ? "Hanyang Univ." : "Yongdap");
+	return;
+};
