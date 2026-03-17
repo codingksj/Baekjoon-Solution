94a604362e1a04d8dd3163c89d02240a5bf38eb8
codingksj <45532564+codingksj@users.noreply.github.com>
2026-03-17 21:05:02 +0900

    Update 15681.cpp

--- 백준/15000~15999/15600~15699/15681.cpp
+++ 백준/15000~15999/15600~15699/15681.cpp
@@ -1,221 +1,90 @@
 #include<bits/stdc++.h>
+#include<ranges>
+#include<print>
 
-// ---------- 사용자 정의 자료형 ----------
 using namespace std;
 
-typedef long long LL;
-typedef unsigned long long ULL;
-typedef long double LD;
+using LL = long long;
+using ULL = unsigned long long;
+using Pii = pair<int, int>;
+using PLL = pair<LL, LL>;
+template<typename T> using V1 = vector<T>;
+template<typename T> using V2 = V1<V1<T>>;
+template<typename T> using V3 = V1<V2<T>>;
 
-typedef pair<char, char> Pcc;
-typedef pair<char, int> Pci;
-typedef pair<char, string> PcS;
-typedef pair<int, bool> Pib;
-typedef pair<int, int> Pii;
-typedef pair<int, char> Pic;
-typedef pair<int, double> Pid;
-typedef pair<int, string> PiS;
-typedef pair<double, double> Pdd;
-typedef pair<double, int> Pdi;
-typedef pair<LL, LL> PLL;
-typedef pair<string, int> PSi;
-typedef pair<string, LL> PSL;
-typedef pair<string, string> PSS;
+namespace CONSTS {
+	constexpr string_view SEPS = " \n";
+	constexpr Pii DT4[4] = { {-1,0}, {0,1} ,{1,0} ,{0,-1} };
+	constexpr ULL MOD = 1E+9 + 7, INF = 0x3f3f3f3f;
+	constexpr int DIGITS = 10, ALPHABETS = 26, MAX = 1E+5;
+	constexpr auto Rng = views::iota;
+	constexpr auto Step = views::stride;
+}
+using namespace CONSTS;
 
-typedef tuple<int, int, int> Tiii;
-
-typedef vector<int> Vi;
-typedef vector<Vi> VVi;
-typedef vector<LL> VLL;
-typedef vector<VLL> VVLL;
-typedef vector<bool> Vb;
-typedef vector<Vb> VVb;
-typedef vector<string> Words;
-typedef vector<Pii> VPii;
-typedef vector<VPii> VVPii;
-
-// ---------- 템플릿 자료형 ----------
-
-// ---------- 템플릿 함수 ----------
-template<typename T> vector<T> InitVector(const size_t SIZE, const T INIT) {
-    return vector<T>(SIZE, INIT);
-}
-template<typename T> vector<vector<T>> Init2DVector(const size_t R, const size_t C, const T INIT) {
-    return vector<vector<T>>(R, InitVector(C, INIT));
-}
-template<typename T> vector<vector<vector<T>>> Init3DVector(const size_t H, const size_t R, const size_t C, const T INIT) {
-    return vector<vector<vector<T>>>(H, Init2DVector(R, C, INIT));
-}
-
-template<typename T1, typename T2> pair<T1, T2> LoadPair() {
-    pair<T1, T2> p;
-    cin >> p.first >> p.second;
-    return p;
-};
-template<typename T> vector<T> LoadVector(const size_t SIZE) {
-    vector<T> V(SIZE);
-    for (T& e : V) {
-        cin >> e;
-    }
-    return V;
-}
-template<typename T1, typename T2> vector<pair<T1, T2>> LoadVector(const size_t SIZE) {
-    vector<pair<T1, T2>> V(SIZE);
-    for (pair<T1, T2>& p : V) {
-        p = LoadPair<T1, T2>();
-    }
-    return V;
-}
-template<typename T> vector<vector<T>> Load2DVector(const size_t R, const size_t C) {
-    vector<vector<T>> V(R, vector<T>(C));
-    for (vector<T>& row : V) {
-        row = LoadVector<T>(C);
-    }
-    return V;
-}
-template<typename T1, typename T2> vector<vector<pair<T1, T2>>> Load2DVector(const size_t R, const size_t C) {
-    vector<vector<pair<T1, T2>>> V(R, vector<pair<T1, T2>>(C));
-    for (vector<pair<T1, T2>>& row : V) {
-        row = LoadVector<T1, T2>(C);
-    }
-    return V;
-}
-
-template<typename T1, typename T2> void PrintPair(const pair<T1, T2>& p, const string sepsPair) {
-    cout << p.first << sepsPair << p.second;
-}
-template<typename T> void PrintVector(const vector<T>& V, const string sepsC) {
-    for (const T& e : V) {
-        cout << e << sepsC;
-    }
-}
-template<typename T1, typename T2> void PrintVector(const vector<pair<T1, T2>>& V, const string sepsR, const string sepsPair) {
-    for (const pair<T1, T2>& p : V) {
-        PrintPair<T1, T2>(p, sepsPair);
-        cout << sepsR;
-    }
-}
-template<typename T> void Print2DVector(const vector<vector<T>>& V, const string sepsR, const string sepsC) {
-    for (const vector<T>& row : V) {
-        PrintVector(row, sepsC);
-        cout << sepsR;
-    }
-}
-template<typename T1, typename T2> void Print2DVector(const vector<vector<pair<T1, T2>>>& V, const string sepsR, const string sepsC, const string sepsPair) {
-    for (const vector<pair<T1, T2>>& row : V) {
-        PrintVector<T1, T2>(row, sepsC, sepsPair);
-        cout << sepsR;
-    }
-}
-
-template<typename T, typename Compare = less<T>> void SortAll(vector<T>& V, Compare cmp = Compare()) {
-    sort(V.begin(), V.end(), cmp);
-}
-template<typename T, typename Compare = less<T>> void SortAll2D(vector<vector<T>>& V, Compare cmp = Compare()) {
-    for (vector<T>& row : V) {
-        Sort(row, cmp);
-    }
-}
-
-template<typename T> size_t LBIndex(const vector<T>& V, const T target, const size_t ds = 0, const size_t de = 0) {
-    return lower_bound(V.begin() + ds, V.end() - de, target) - V.begin();
-}
-template<typename T> size_t UBIndex(const vector<T>& V, const T target, const size_t ds = 0, const size_t de = 0) {
-    return upper_bound(V.begin() + ds, V.end() - de, target) - V.begin();
-}
-template<typename T> size_t CountByBound(const vector<T>& V, const T leftVal, const T rightVal) {
-    return upper_bound(V.begin(), V.end(), rightVal) - lower_bound(V.begin(), V.end(), leftVal);
-};
-
-template<typename Number> vector<Number> InitArithmeticSeq(const size_t N, const Number firstTerm = 1, const Number commonDiff = 1) {
-    vector<Number> seq(N);
-
-    for (size_t i = 0; i < N; i++) {
-        seq[i] = firstTerm + i * commonDiff;
-    }
-
-    return seq;
-}
-
-// ---------- 상수 (열거형 등) ----------
-const LD PI = acos(-1);
-const LD EPS = 1E-8;
-const LL MILLION = 1E+6;
-const LL BILLION = 1E+9;
-const LL TRILLION = 1E+12;
-const LL INF = 1E+8;
-const int MAX = 2E+5;
-
-// ---------- 사용자 정의 함수 및 연산자 ----------
 void FastIO();
+void SetUp();
 bool Input();
 void Solve();
-int DFS(const int v);
+int DFS(int v);
 void Output();
 
-// ---------- 전역 변수 ----------
-VVi graph;
-Vi sz;
-int N, R, Q, u, v;
-Vb dfsChk;
+V1<int> G[MAX];
+int sz[MAX];
+int visChk[MAX];
+int N, R, Q, v1, v2;
 
 int main() {
-    FastIO();
+	FastIO();
+	SetUp();
 
-    Input();
-    Solve();
-    Output();
+	Input();
+	Solve();
+	Output();
 
-    return 0;
+	return 0;
 }
 
 void FastIO() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+	ios::sync_with_stdio(false); cin.tie(nullptr);
+	return;
+};
+
+void SetUp() {
+	return;
 };
 
 bool Input() {
-    cin >> N >> R >> Q;
+	cin >> N >> R >> Q;
 
-    graph = VVi(N + 1, Vi(0));
-    sz = Vi(N + 1, 0);
-    dfsChk = Vb(N + 1, false);
+	for (int i : Rng(0, N - 1)) {
+		cin >> v1 >> v2;
+		G[--v1].push_back(--v2);
+		G[v2].push_back(v1);
+	}
 
-    for (int i = 0; i < N - 1; i++) {
-        cin >> u >> v;
-
-        graph[u].push_back(v);
-        graph[v].push_back(u);
-    }
-
-    return true;
+	return true;
 };
 
 void Solve() {
-    sz[R] = DFS(R);
+	fill(sz, sz + N, 1);
+	sz[R - 1] = DFS(R - 1);
+	while (Q--) {
+		cin >> v1;
+		println("{}", sz[--v1]);
+	}
+};
 
-    for (int q = 0; q < Q; q++) {
-        cin >> v;
-        cout << sz[v] << "\n";
-    }
-}
-
-int DFS(const int v) {
-    if (sz[v]) {
-        return sz[v];
-    }
-    dfsChk[v] = true;
-
-    int res = 1;
-
-    for (const int& nV : graph[v]) {
-        if (!dfsChk[nV]) {
-            res += DFS(nV);
-        }
-    }
-    return sz[v] = res;
+int DFS(int v) {
+	visChk[v] = true;
+	for (int nv : G[v]) {
+		if (!visChk[nv]) {
+			sz[v] += DFS(nv);
+		}
+	}
+	return sz[v];
 };
 
 void Output() {
-
+	return;
 };
