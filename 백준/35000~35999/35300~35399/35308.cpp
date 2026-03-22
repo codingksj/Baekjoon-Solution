9e34b3b15f71d85a167915413d039a23e4d6a39a
codingksj <45532564+codingksj@users.noreply.github.com>
2026-03-22 21:20:43 +0900

    Create 35308.cpp

---
+++ 백준/35000~35999/35300~35399/35308.cpp
@@ -0,0 +1,79 @@
+#include<bits/stdc++.h>
+#include<ranges>
+#include<print>
+
+using namespace std;
+
+using LL = long long;
+using ULL = unsigned long long;
+using Pii = pair<int, int>;
+using PLL = pair<LL, LL>;
+using Ti3 = tuple<int, int, int>;
+template<typename T> using V1 = vector<T>;
+template<typename T> using V2 = V1<V1<T>>;
+template<typename T> using V3 = V1<V2<T>>;
+
+namespace CONSTS {
+	constexpr string_view SEPS = " \n";
+	constexpr Pii DT4[4] = { {-1,0}, {0,1} ,{1,0} ,{0,-1} };
+	constexpr ULL MOD = 1E+9 + 7, INF = 0x3f3f3f3f;
+	constexpr int DIGITS = 10, ALPHABETS = 26, MAX = 1E+5;
+	constexpr auto Rng = views::iota;
+	constexpr auto Step = views::stride;
+}
+using namespace CONSTS;
+
+void FastIO();
+void SetUp();
+bool Input();
+void Solve();
+void Output();
+
+int TC, N, K;
+
+int main() {
+	FastIO();
+	SetUp();
+
+	cin >> TC;
+
+	while (TC--) {
+		Input();
+		Solve();
+		Output();
+	}
+
+	return 0;
+}
+
+void FastIO() {
+	ios::sync_with_stdio(false); cin.tie(nullptr);
+	return;
+};
+
+void SetUp() {
+	return;
+};
+
+bool Input() {
+	cin >> N >> K;
+
+	return true;
+};
+
+void Solve() {
+	return;
+};
+
+void Output() {
+	if (N > 1 && K != 2) {
+		println("-1");
+	}
+	else {
+		for (int i = 1; i < N; i++) {
+			print("{} ", i + 1);
+		}
+		println("{}", 1);
+	}
+	return;
+};
