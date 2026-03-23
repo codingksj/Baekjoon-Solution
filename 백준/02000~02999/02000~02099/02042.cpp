3a453cc2df72590af1dee97a2c08abcbe42363e1
codingksj <45532564+codingksj@users.noreply.github.com>
2026-03-23 18:55:05 +0900

    Update 02042.cpp

--- 백준/02000~02999/02000~02099/02042.cpp
+++ 백준/02000~02999/02000~02099/02042.cpp
@@ -1,247 +1,100 @@
 #include<bits/stdc++.h>
+#include<ranges>
+#include<print>
 
-// ---------- 사용자 정의 상수 ----------
-#define PI 3.141592653589793
-#define THOUSAND 1E+3
-#define MILLION 1E+6
-#define BILLION 1E+9
-#define TRILLION 1E+12
-#define INF 1E+8
-#define EPSILON 1E-9
-
-// ---------- 사용자 정의 자료형 ----------
 using namespace std;
 
-typedef long long LL;
-typedef unsigned long long ULL;
-typedef long double LD;
+using LL = long long;
+using ULL = unsigned long long;
+using Pii = pair<int, int>;
+using PLL = pair<LL, LL>;
+template<typename T> using V1 = vector<T>;
+template<typename T> using V2 = V1<V1<T>>;
+template<typename T> using V3 = V1<V2<T>>;
 
-typedef pair<char, char> Pcc;
-typedef pair<char, int> Pci;
-typedef pair<char, string> PcS;
-typedef pair<int, bool> Pib;
-typedef pair<int, int> Pii;
-typedef pair<int, char> Pic;
-typedef pair<int, double> Pid;
-typedef pair<int, string> PiS;
-typedef pair<double, double> Pdd;
-typedef pair<double, int> Pdi;
-typedef pair<LL, LL> PLL;
-typedef pair<string, int> PSi;
-typedef pair<string, LL> PSL;
-typedef pair<string, string> PSS;
-
-typedef tuple<int, int, int> Tiii;
-
-typedef vector<int> Vi;
-typedef vector<Vi> VVi;
-typedef vector<string> Words;
-
-// ---------- 템플릿 자료형 ----------
-template<typename T> struct Matrix2D {
-    vector<vector<T>> m2;
-
-    Matrix2D() {};
-    Matrix2D(const size_t R, const size_t C, const T INIT) {
-        m2 = vector<vector<T>>(R, vector<T>(C, INIT));
-    }
-};
-template<typename T> struct Matrix3D {
-    vector<vector<vector<T>>> m3;
-
-    Matrix3D() {};
-    Matrix3D(const size_t H, const size_t R, const size_t C, const T INIT) {
-        m3 = vector<vector<vector<T>>>(H, vector<vector<T>>(R, vector<T>(C, INIT)));
-    }
-};
-
-template<typename Number> struct Point2D {
-    Number x;
-    Number y;
-
-    Point2D() {};
-    Point2D(const Number x, const Number y) : x(x), y(y) {};
-    Point2D(const pair<Number, Number> p) {
-        x = p.first;
-        y = p.second;
-    }
-};
-
-
-// ---------- 템플릿 함수 ----------
-template<typename T> vector<T> InitVector(const size_t SIZE, const T INIT) {
-    return vector<T>(SIZE, INIT);
+namespace CONSTS {
+    constexpr string_view SEPS = " \n";
+    constexpr Pii DT4[4] = { {-1,0}, {0,1} ,{1,0} ,{0,-1} };
+    constexpr ULL MOD = 1E+9 + 7, INF = 0x3f3f3f3f;
+    constexpr int DIGITS = 10, ALPHABETS = 26, MAX = 21;
+    constexpr auto Rng = views::iota;
+    constexpr auto Step = views::stride;
 }
-template<typename T> vector<vector<T>> Init2DVector(const size_t R, const size_t C, const T INIT) {
-    return vector<vector<T>>(R, InitVector(C, INIT));
-}
-template<typename T> vector<vector<vector<T>>> Init3DVector(const size_t H, const size_t R, const size_t C, const T INIT) {
-    return vector<vector<vector<T>>>(H, Init2DVector(R, C, INIT));
-}
+using namespace CONSTS;
 
-template<typename T1, typename T2> pair<T1, T2> LoadPair() {
-    pair<T1, T2> p;
-    cin >> p.first >> p.second;
-    return p;
-};
-template<typename T> vector<T> LoadVector(const size_t SIZE) {
-    vector<T> V(SIZE);
-    for (T& e : V) {
-        cin >> e;
-    }
-    return V;
-}
-template<typename T1, typename T2> vector<pair<T1, T2>> LoadVector(const size_t SIZE) {
-    vector<pair<T1, T2>> V(SIZE);
-    for (pair<T1, T2>& p : V) {
-        p = LoadPair<T1, T2>();
-    }
-    return V;
-}
-template<typename T> vector<vector<T>> Load2DVector(const size_t R, const size_t C) {
-    vector<vector<T>> V(R, vector<T>(C));
-    for (vector<T>& row : V) {
-        row = LoadVector<T>(C);
-    }
-    return V;
-}
-template<typename T1, typename T2> vector<vector<pair<T1, T2>>> Load2DVector(const size_t R, const size_t C) {
-    vector<vector<pair<T1, T2>>> V(R, vector<pair<T1, T2>>(C));
-    for (vector<pair<T1, T2>>& row : V) {
-        row = LoadVector<T1, T2>(C);
-    }
-    return V;
-}
+void FastIO();
+void SetUp();
+bool Input();
+void Solve();
+void Output();
 
-template<typename T1, typename T2> void PrintPair(const pair<T1, T2>& p, const string sepsPair) {
-    cout << p.first << sepsPair << p.second;
-}
-template<typename T> void PrintVector(const vector<T>& V, const string sepsC) {
-    for (const T& e : V) {
-        cout << e << sepsC;
-    }
-}
-template<typename T1, typename T2> void PrintVector(const vector<pair<T1, T2>>& V, const string sepsR, const string sepsPair) {
-    for (const pair<T1, T2>& p : V) {
-        PrintPair<T1, T2>(p, sepsPair);
-        cout << sepsR;
-    }
-}
-template<typename T> void Print2DVector(const vector<vector<T>>& V, const string sepsR, const string sepsC) {
-    for (const vector<T>& row : V) {
-        PrintVector(row, sepsC);
-        cout << sepsR;
-    }
-}
-template<typename T1, typename T2> void Print2DVector(const vector<vector<pair<T1, T2>>>& V, const string sepsR, const string sepsC, const string sepsPair) {
-    for (const vector<pair<T1, T2>>& row : V) {
-        PrintVector<T1, T2>(row, sepsC, sepsPair);
-        cout << sepsR;
-    }
-}
-
-template<typename T, typename Compare = less<T>> void SortAll(vector<T>& V, Compare cmp = Compare()) {
-    sort(V.begin(), V.end(), cmp);
-}
-template<typename T, typename Compare = less<T>> void SortAll2D(vector<vector<T>>& V, Compare cmp = Compare()) {
-    for (vector<T>& row : V) {
-        Sort(row, cmp);
-    }
-}
-template<typename T, typename Compare = less<T>> void StableSortAll(vector<T>& V, Compare cmp = Compare()) {
-    stable_sort(V.begin(), V.end(), cmp);
-}
-template<typename T, typename Compare = less<T>> void StableSortAll2D(vector<vector<T>>& V, Compare cmp = Compare()) {
-    for (vector<T>& row : V) {
-        StableSort(row, cmp);
-    }
-}
-
-template<typename T> size_t LowerBoundIndex(const vector<T>& V, const T target) {
-    return lower_bound(V.begin(), V.end(), target) - V.begin();
-}
-template<typename T> size_t UpperBoundIndex(const vector<T>& V, const T target) {
-    return upper_bound(V.begin(), V.end(), target) - V.begin();
-}
-
-template<typename Number> vector<Number> InitArithmeticSeq(const size_t N, const Number firstTerm = 1, const Number commonDiff = 1) {
-    vector<Number> seq(N);
-
-    for (size_t i = 0; i < N; i++) {
-        seq[i] = firstTerm + i * commonDiff;
-    }
-
-    return seq;
-}
-template<typename Number> Number CCW(const Point2D<Number>& p1, const Point2D<Number>& p2, const Point2D<Number>& p3) {
-    return (p2.x - p1.x) * (p3.y - p1.y) - (p3.x - p1.x) * (p2.y - p1.y);
-}
-
-// ---------- 사용자 정의 함수 및 연산자 ----------
-LL Init(const int node, const int start, const int end);
-LL Query(const int node, const int start, const int end, const int left, const int right);
-void Update(const int node, const int start, const int end, const int idx, const LL diff);
-
-
-// ---------- 열거형 상수 ----------
-
-// ---------- 전역 변수 ----------
-vector<LL> seq, tree;
-LL a, b, c;
-int N, M, K;
+V1<LL> tree;
+LL a, b;
+int N, M, K, Q, sz, q;
 
 int main() {
-    cin.tie(nullptr);
-    ios::sync_with_stdio(false);
+    FastIO();
+    SetUp();
 
-    cin >> N >> M >> K;
-
-    seq = LoadVector<LL>(N);
-    tree = InitVector<LL>(4 * N, 0);
-
-    Init(1, 0, N - 1);
-
-    for (int q = 0; q < M + K; q++) {
-        cin >> a >> b >> c;
-
-        if (a == 1) {
-            Update(1, 0, N - 1, b - 1, c - seq[b - 1]);
-            seq[b - 1] = c;
-        }
-        else {
-            cout << Query(1, 0, N - 1, b - 1, c - 1) << "\n";
-        }
-    }
+    Input();
+    Solve();
+    Output();
 
     return 0;
 }
 
-LL Init(const int node, const int start, const int end) {
-    return tree[node] = ((start == end) ? seq[start] : Init(2 * node, start, (start + end) / 2) + Init(2 * node + 1, (start + end) / 2 + 1, end));
+void FastIO() {
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    return;
 };
 
-LL Query(const int node, const int start, const int end, const int left, const int right) {
-    if (left > end || right < start) {
-        return 0;
-    }
-    if (left <= start && end <= right) {
-        return tree[node];
-    }
-    int mid = (start + end) / 2;
-
-    return Query(2 * node, start, mid, left, right) + Query(2 * node + 1, mid + 1, end, left, right);
+void SetUp() {
+    return;
 };
 
-void Update(const int node, const int start, const int end, const int idx, const LL diff) {
-    if (idx<start || idx>end) {
-        return;
-    }
-    tree[node] += diff;
+bool Input() {
+    cin >> N >> M >> K;
 
-    int mid = (start + end) / 2;
+    sz = bit_ceil(ULL(N));
 
-    if (start != end) {
-        Update(2 * node, start, mid, idx, diff);
-        Update(2 * node + 1, mid + 1, end, idx, diff);
+    tree.resize(2 * sz);
+
+    for (int i : Rng(0, N)) {
+        cin >> tree[i + sz];
     }
+    return true;
+};
+
+void Solve() {
+    for (int i = sz - 1;i > 0;i--) {
+        tree[i] = tree[i << 1] + tree[i << 1 | 1];
+    }
+    auto Update = [](int i, LL v) {
+        tree[i += sz] = v;
+        for (i >>= 1; i > 0;i >>= 1) {
+            tree[i] = tree[i << 1] + tree[i << 1 | 1];
+        }
+    };
+    auto Query = [](int l, int r) {
+        LL res = 0;
+        for (l += sz, r += sz; l < r; l >>= 1, r >>= 1) {
+            (l & 1) && (res += tree[l++]);
+            (r & 1) && (res += tree[--r]);
+        }
+        return res;
+    };
+    Q = M + K;
+    while (Q--) {
+        cin >> q >> a >> b;
+        if (q == 1) {
+            Update(--a, b);
+        }
+        else {
+            println("{}", Query(--a, b));
+        }
+    }
+};
+
+void Output() {
+    return;
 };
