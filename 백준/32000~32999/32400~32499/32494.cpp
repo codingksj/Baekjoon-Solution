ad44cc2f34be12945ffbbe81b630251790503a0e
codingksj <45532564+codingksj@users.noreply.github.com>
2026-03-28 19:56:52 +0900

    Update 32494.cpp

--- 백준/32000~32999/32400~32499/32494.cpp
+++ 백준/32000~32999/32400~32499/32494.cpp
@@ -29,16 +29,17 @@ void FastIO();
 void SetUp();
 bool Input();
 void Solve();
+int Find(int v);
+void Union(int u, int v, int w);
 void Output();
 
-V1<PLL> A;
 V1<Ti3> edges;
-LL sz[MAX + 1];
+V1<PLL> A;
 LL counts[MAX + 1];
-LL pSum[MAX + 1];
+LL sz[MAX + 1];
 int par[MAX + 1];
 int vals[MAX + 1];
-int N, Q, u, v, w;
+int N, Q, u, v, w, cnt;
 
 int main() {
     FastIO();
@@ -68,48 +69,47 @@ bool Input() {
     for (auto& [w, u, v] : edges) {
         cin >> u >> v >> w;
     }
-
     return true;
 };
 
 void Solve() {
-    iota(par + 1, par + N + 1, 1);
-    ranges::fill(sz, sz + N + 1, 1);
+    for (int i : Rng(1, MAX + 1)) {
+        par[i] = i, sz[i] = 1;
+    }
     ranges::sort(edges);
-    auto Find = [](auto&& self, int v) -> int {
-        return v == par[v] ? v : par[v] = self(self, par[v]);
-    };
-    auto Union = [&](int u, int v, int w) {
-        u = Find(Find, u);
-        v = Find(Find, v);
-        if (u < v) {
-            swap(u, v);
-        }
-        par[u] = v;
-        A.emplace_back(w, sz[u] * sz[v]);
-        sz[v] += sz[u];
-    };
     for (const auto& [w, u, v] : edges) {
         Union(u, v, w);
     }
     ranges::sort(A);
-    vals[1] = A[0].first;
-    int cnt = 1;
+    int cnt = 0;
     for (const auto& [w, n] : A) {
-        if (w != vals[cnt]) {
-            vals[++cnt] = w;
-        }
+        (w != vals[cnt]) && (vals[++cnt] = w);
         counts[cnt] += n;
     }
     for (int i = 1; i <= cnt; i++) {
-        pSum[i] = pSum[i - 1] + counts[i] * vals[i];
+        counts[i] *= vals[i];
+        counts[i] += counts[i - 1];
     }
     while (Q--) {
         cin >> u >> v;
         int i1 = ranges::lower_bound(vals + 1, vals + cnt + 1, u) - vals;
         int i2 = ranges::upper_bound(vals + 1, vals + cnt + 1, v) - vals - 1;
-        println("{}", pSum[i2] - pSum[i1 - 1]);
+        println("{}", counts[i2] - counts[i1 - 1]);
     }
+};
+
+int Find(int v) {
+    return v == par[v] ? v : par[v] = Find(par[v]);
+};
+
+void Union(int u, int v, int w) {
+    u = Find(u), v = Find(v);
+    if (u < v) {
+        swap(u, v);
+    }
+    par[u] = v;
+    A.emplace_back(w, sz[u] * sz[v]);
+    sz[v] += sz[u];
 };
 
 void Output() {
