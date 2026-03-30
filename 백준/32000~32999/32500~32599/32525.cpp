1faa8641269f475740c754e3c4938d2c39d2d72b
codingksj <45532564+codingksj@users.noreply.github.com>
2026-03-31 00:54:10 +0900

    Create 32525.cpp

---
+++ 백준/32000~32999/32500~32599/32525.cpp
@@ -0,0 +1,80 @@
+#include<bits/stdc++.h>
+#include<ranges>
+#include<print>
+
+using namespace std;
+
+using LL = long long;
+using ULL = unsigned long long;
+using Pii = pair<int, int>;
+using PLL = pair<LL, LL>;
+using Ti3 = tuple<int, int, int>;
+template<typename T> using V1 = vector<T>;
+template<typename T> using V2 = V1<V1<T>>;
+template<typename T> using V3 = V1<V2<T>>;
+
+namespace CONSTS {
+	constexpr string_view SEPS = " \n";
+	constexpr Pii DT4[4] = { {-1,0}, {0,1} ,{1,0} ,{0,-1} };
+	constexpr ULL MOD = 1E+9 + 7, INF = 0x3f3f3f3f;
+	constexpr int DIGITS = 10, ALPHABETS = 26, MAX = 2E+8 + 1, MAX_N = 100;
+	constexpr auto Rng = views::iota;
+	constexpr auto Rev = views::reverse;
+	constexpr auto Split = views::split;
+}
+using namespace CONSTS;
+
+void FastIO();
+void SetUp();
+bool Input();
+void Solve();
+void Output();
+
+int X[MAX_N];
+int Y[MAX_N];
+int TC, N;
+
+int main() {
+	FastIO();
+	SetUp();
+
+	cin >> TC;
+
+	while(TC--){
+		Input();
+		Solve();
+		Output();
+	}
+
+	return 0;
+}
+
+void FastIO() {
+	ios::sync_with_stdio(false); cin.tie(nullptr);
+	return;
+};
+
+void SetUp() {
+	return;
+};
+
+bool Input() {
+	cin >> N;
+
+	for (int i : Rng(0, N)) {
+		cin >> X[i] >> Y[i];
+	}
+
+	return true;
+};
+
+void Solve() {
+	return;
+};
+
+void Output() {
+	for (int i : Rng(0, N)) {
+		println("{} {} {}", i + 1, X[i] + 1, Y[i] + MAX);
+	}
+	return;
+};
