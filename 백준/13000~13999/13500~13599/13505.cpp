98e00483c54840da0600dfa3a92fe46b56b46127
codingksj <45532564+codingksj@users.noreply.github.com>
2026-04-04 15:13:11 +0900

    Update 13505.cpp

--- 백준/13000~13999/13500~13599/13505.cpp
+++ 백준/13000~13999/13500~13599/13505.cpp
@@ -1,5 +1,4 @@
 #include<bits/stdc++.h>
-#include<unordered_set>
 #include<ranges>
 #include<print>
 
@@ -8,17 +7,20 @@ using namespace std;
 using LL = long long;
 using ULL = unsigned long long;
 using Pii = pair<int, int>;
+using PLL = pair<LL, LL>;
+using Ti3 = tuple<int, int, int>;
 template<typename T> using V1 = vector<T>;
 template<typename T> using V2 = V1<V1<T>>;
 template<typename T> using V3 = V1<V2<T>>;
 
 namespace CONSTS {
-    constexpr string_view SEPS = " \n";
-    constexpr Pii DT4[4] = { {-1,0}, {0,1} ,{1,0} ,{0,-1} };
-    constexpr ULL MOD = 1E+9 + 7, INF = 0x3f3f3f3f, MAX = 1E+5;
-    constexpr int DIGITS = 10, ALPHABETS = 26;
-    constexpr auto Rng = views::iota;
-    constexpr auto Step = views::stride;
+	constexpr string_view SEPS = " \n";
+	constexpr Pii DT4[4] = { {-1,0}, {0,1} ,{1,0} ,{0,-1} };
+	constexpr LL MOD = 1E+9 + 7, INF = 0x3f3f3f3f;
+	constexpr int DIGITS = 10, ALPHABETS = 26, MAX_NODE = 32E+5, MAX = 1E+5;
+	constexpr auto Rng = views::iota;
+	constexpr auto Rev = views::reverse;
+	constexpr auto Split = views::split;
 }
 using namespace CONSTS;
 
@@ -28,78 +30,73 @@ bool Input();
 void Solve();
 void Output();
 
-struct Trie {
-    Trie* childs[2];
-
-    Trie() {
-        childs[0] = childs[1] = nullptr;
-    }
-    void Insert(int n) {
-        Trie* cur = this;
-        for (int i = 31; i >= 0; i--) {
-            int b = (n >> i) & 1;
-            if (cur->childs[b] == nullptr) {
-                cur->childs[b] = new Trie;
-            }
-            cur = cur->childs[b];
-        }
-    }
-    int Find(int n) {
-        Trie* cur = this;
-        int res = 0;
-        for (int i = 31; i >= 0; i--) {
-            int b = (~n >> i) & 1;
-            if (cur->childs[b] == nullptr) {
-                b = !b;
-            }
-            res |= b << i;
-            cur = cur->childs[b];
-        }
-        return n ^ res;
-    };
-};
-Trie* root = new Trie;
-int A[MAX + 1];
-int N, n, mxN;
+int node[MAX_NODE + 1][2];
+int N, X, nodeCnt = 1;
 
 int main() {
-    FastIO();
-    SetUp();
+	FastIO();
+	SetUp();
 
-    Input();
-    Solve();
-    Output();
+	Input();
+	Solve();
+	Output();
 
-    return 0;
+	return 0;
 }
 
 void FastIO() {
-    ios::sync_with_stdio(false); cin.tie(nullptr);
-    return;
+	ios::sync_with_stdio(false); cin.tie(nullptr);
+	return;
 };
 
 void SetUp() {
-    return;
+	return;
 };
 
 bool Input() {
-    cin >> N;
+	cin >> N;
 
-    for (int i : Rng(0, N)) {
-        cin >> A[i];
-        root->Insert(A[i]);
-    }
-
-    return true;
+	return true;
 };
 
 void Solve() {
-    for (int i : Rng(0, N)) {
-        mxN = max(mxN, root->Find(A[i]));
-    }
+	auto Insert = [&](int n) {
+		int cur = 0;
+		for (int i = 30; i >= 0;i--) {
+			int b = (n >> i) & 1;
+			int& child = node[cur][b];
+			if (!child) {
+				node[nodeCnt][0] = node[nodeCnt][1] = 0;
+				child = nodeCnt++;
+			}
+			cur = child;
+		}
+	};
+	auto Find = [&](int n) {
+		int cur = 0;
+		int res = 0;
+		for (int i = 30; i >= 0; i--) {
+			bool t = (~n >> i) & 1;
+			int& child = node[cur][t];
+			if (child) {
+				res |= (1 << i);
+				cur = child;
+			}
+			else {
+				cur = node[cur][!t];
+			}
+		}
+		return res;
+	};
+	for (int i = 0, n; i < N; i++) {
+		cin >> n;
+		Insert(n);
+		X = max(X, Find(n));
+	}
+	return;
 };
 
 void Output() {
-    println("{}", mxN);
-    return;
+	println("{}", X);
+	return;
 };
