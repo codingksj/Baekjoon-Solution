119f8ada05e09b24594db20ccf655edd2b718ecc
codingksj <45532564+codingksj@users.noreply.github.com>
2026-04-04 17:23:30 +0900

    Update 10840.cpp

--- 백준/10000~10999/10800~10899/10840.cpp
+++ 백준/10000~10999/10800~10899/10840.cpp
@@ -1,8 +1,7 @@
 #include<bits/stdc++.h>
-#include<unordered_set>
 #include<ranges>
-#include<numbers>
 #include<print>
+#include<random>
 
 using namespace std;
 
@@ -10,19 +9,19 @@ using LL = long long;
 using ULL = unsigned long long;
 using Pii = pair<int, int>;
 using PLL = pair<LL, LL>;
-using Node = tuple<ULL, ULL, ULL, ULL>;
+using Ti3 = tuple<int, int, int>;
 template<typename T> using V1 = vector<T>;
 template<typename T> using V2 = V1<V1<T>>;
 template<typename T> using V3 = V1<V2<T>>;
 
 namespace CONSTS {
-    constexpr string_view SEPS = " \n";
-    constexpr string_view target = "long";
-    constexpr Pii DT4[4] = { {-1,0}, {0,1} ,{1,0} ,{0,-1} };
-    constexpr ULL MAX = 1501, MOD = 7, INF = 0x3f3f3f3f;
-    constexpr int DIGITS = 10, ALPHABETS = 26;
-    constexpr auto Rng = views::iota;
-    constexpr auto Step = views::stride;
+	constexpr string_view SEPS = " \n";
+	constexpr Pii DT4[4] = { {-1,0}, {0,1} ,{1,0} ,{0,-1} };
+	constexpr LL MOD = 1E+9 + 7, INF = 0x3f3f3f3f;
+	constexpr int DIGITS = 10, ALPHABETS = 26, MAX = 1E+6;
+	constexpr auto Rng = views::iota;
+	constexpr auto Rev = views::reverse;
+	constexpr auto Split = views::split;
 }
 using namespace CONSTS;
 
@@ -32,71 +31,92 @@ bool Input();
 void Solve();
 void Output();
 
-map<Node, int> hashS;
-string s, t;
-ULL pows[1 << 3] = {1ULL, };
-int N, M, mxL;
+random_device rd;
+mt19937 gen(rd());
+string T, P;
+ULL w[2][ALPHABETS];
+int res;
 
 int main() {
-    FastIO();
-    SetUp();
+	FastIO();
+	SetUp();
 
-    Input();
-    Solve();
-    Output();
+	Input();
+	Solve();
+	Output();
 
-    return 0;
+	return 0;
 }
 
 void FastIO() {
-    ios::sync_with_stdio(false); cin.tie(nullptr);
-    return;
+	ios::sync_with_stdio(false); cin.tie(nullptr);
+	return;
 };
 
 void SetUp() {
-    for (int i = 1; i <= 6; i++) {
-        pows[i] = pows[i - 1] * MAX;
-    }
-    return;
+	set<ULL> used;
+	for (int t : Rng(0, 2)) {
+		for (int i = 0; i < ALPHABETS; i++) {
+			ULL val = 0;
+			do {
+				val = gen();
+			} while (!val || used.count(val));
+			w[t][i] = val;
+			used.insert(val);
+		}
+	}
+	return;
 };
 
 bool Input() {
-    cin >> s >> t;
+	getline(cin, T);
+	getline(cin, P);
 
-    return true;
+	return true;
 };
 
 void Solve() {
-    N = s.length();
-    M = t.length();
-    for (int i = 0; i < N; i++) {
-        ULL v[4] = { 0, };
-        for (int j = i; j < N; j++) {
-            int alpha = s[j] - 'a';
-            int idx = (alpha / MOD);
-            int exp = alpha % MOD;
-            v[idx] += pows[exp];
-            Node node = { v[0], v[1], v[2], v[3] };
-            hashS.insert({ node, j - i + 1 });
-        }
-    }
-    for (int i = 0; i < M; i++) {
-        ULL v[4] = { 0, };
-        for (int j = i; j < M; j++) {
-            int alpha = t[j] - 'a';
-            int idx = (alpha / MOD);
-            int exp = alpha % MOD;
-            v[idx] += pows[exp];
-            Node node = { v[0], v[1], v[2], v[3] };
-            if (hashS.count(node)) {
-                mxL = max(mxL, j - i + 1);
-            }
-        }
-    }
-    return;
+	if (T.size() < P.size()) {
+		swap(T, P);
+	}
+	int tN = T.size();
+	int pN = P.size();
+
+	for (res = pN; res; res--) {
+		set<pair<ULL, ULL>> lookUp;
+		ULL h1 = 0, h2 = 0;
+		for (int i = 0; i < res - 1; i++) {
+			h1 += w[0][T[i] - 'a'];
+			h2 += w[1][T[i] - 'a'];
+		}
+		for (int i = res - 1; i < tN; i++) {
+			h1 += w[0][T[i] - 'a'];
+			h2 += w[1][T[i] - 'a'];
+			lookUp.insert({ h1, h2 });
+			h1 -= w[0][T[i - (res - 1)] - 'a'];
+			h2 -= w[1][T[i - (res - 1)] - 'a'];
+		}
+		ULL p1 = 0, p2 = 0;
+		for (int i = 0; i < res - 1; i++) {
+			p1 += w[0][P[i] - 'a'];
+			p2 += w[1][P[i] - 'a'];
+		}
+		bool found = false;
+		for (int i = res - 1; i < pN; i++) {
+			p1 += w[0][P[i] - 'a'];
+			p2 += w[1][P[i] - 'a'];
+			found |= lookUp.count({ p1, p2 });
+			p1 -= w[0][P[i - (res - 1)] - 'a'];
+			p2 -= w[1][P[i - (res - 1)] - 'a'];
+		}
+		if (found) {
+			return;
+		}
+	}
+	return;
 };
 
 void Output() {
-    println("{}", mxL);
-    return;
+	println("{}", res);
+	return;
 };
