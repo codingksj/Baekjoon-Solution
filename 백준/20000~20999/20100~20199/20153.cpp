742c0e0fb6d7e94e89a33c8d95237288928f3be2
codingksj <45532564+codingksj@users.noreply.github.com>
2026-04-10 23:56:03 +0900

    Create 20153.cpp

---
+++ 백준/20000~20999/20100~20199/20153.cpp
@@ -0,0 +1,80 @@
+#include<bits/stdc++.h>
+#include<ranges>
+#include<print>
+
+using namespace std;
+
+using LL = long long;
+using ULL = unsigned long long;
+using Pii = pair<int, int>;
+using PLL = pair<LL, LL>;
+using Ti3 = tuple<int, int, int>;
+using Node = pair<int, char>;
+template<typename T> using V1 = vector<T>;
+template<typename T> using V2 = V1<V1<T>>;
+template<typename T> using V3 = V1<V2<T>>;
+
+namespace CONSTS {
+	constexpr string_view SEPS = " \n";
+	constexpr Pii DT4[4] = { {-1,0}, {0,1} ,{1,0} ,{0,-1} };
+	constexpr LL MOD = 1E+9 + 7, INF = 0x3f3f3f3f;
+	constexpr int DIGITS = 10, ALPHABETS = 26, MAX = 2'222'222;
+	constexpr auto Rng = views::iota;
+	constexpr auto Rev = views::reverse;
+	constexpr auto Split = views::split;
+	constexpr auto Take = views::take;
+}
+using namespace CONSTS;
+
+void FastIO();
+void SetUp();
+bool Input();
+void Solve();
+void Output();
+
+int A[MAX];
+int N, total, mx;
+
+int main() {
+	FastIO();
+	SetUp();
+
+	Input();
+	Solve();
+	Output();
+
+	return 0;
+}
+
+void FastIO() {
+	ios::sync_with_stdio(false); cin.tie(nullptr);
+	return;
+};
+
+void SetUp() {
+	return;
+};
+
+bool Input() {
+	cin >> N;
+
+	for (auto& a : A | Take(N)) {
+		cin >> a;
+		total ^= a;
+	}
+
+	return true;
+};
+
+void Solve() {
+	mx = total;
+	for (const auto& a : A | Take(N)) {
+		mx = max(mx, total ^ a);
+	}
+	return;
+};
+
+void Output() {
+	println("{}{}", mx, mx);
+	return;
+};
